efc3aac25f5db4b0b06351b3cb2a8d1e9d920a1b
David Kraushar <david.kraushar@gmail.com>
2020-07-09 16:30:08 -0600

    Add GoA css and javascript to core-css library. Add a button story to the core-css library.

---
+++ libs/core-css/src/lib/core-css.stories.ts
@@ -0,0 +1,5 @@
+import '@storybook/html';
+import './core-css';
+
+export default { title: 'Button-link' };
+export const card = () => '<div><a href="#" class="goa-link-button">Call To Action</a></div>';
--- libs/core-css/src/lib/core-css.ts
+++ libs/core-css/src/lib/core-css.ts
@@ -1,3 +1,3 @@
-export function coreCss(): string {
-  return 'core-css';
-}
+export const componentsCss = require('./goa-components.css');
+export const coreCss = require('./goa-core.css');
+//export * from './goa.js';
