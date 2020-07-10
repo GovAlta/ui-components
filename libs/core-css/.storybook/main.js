0773d785e48463686743a04d9c16d4f3cb36e41c
David Kraushar <david.kraushar@gmail.com>
2020-07-10 11:00:44 -0600

    Add the fancy addons for storybook.

--- libs/core-css/.storybook/main.js
+++ libs/core-css/.storybook/main.js
@@ -1,6 +1,14 @@
 module.exports = {
     stories: ['../src/lib/**/*.stories.js'],
     addons: [
-      '@storybook/addon-knobs',
-    ],
+      {
+        name: '@storybook/addon-docs',
+        options: {
+        }
+      },
+      '@storybook/addon-backgrounds',
+      '@storybook/addon-a11y/register',
+      '@storybook/addon-viewport',
+      'storybook-addon-xd-designs'
+    ]
 };
