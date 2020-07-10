dbe17bdd4d0cc48d3d089333bd687592aca6634c
David Kraushar <david.kraushar@gmail.com>
2020-07-10 08:05:45 -0600

    Move button to own story.

--- libs/core-css/src/lib/core-css.stories.ts
+++
@@ -1,5 +0,0 @@
-import '@storybook/html';
-import './core-css';
-
-export default { title: 'Button-link' };
-export const card = () => '<div><a href="#" class="goa-link-button">Call To Action</a></div>';
