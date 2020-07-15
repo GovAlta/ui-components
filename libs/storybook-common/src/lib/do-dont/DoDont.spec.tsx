578e082e0fb15531f662b47fb43756a29137fa4a
David Kraushar <david.kraushar@gmail.com>
2020-07-15 08:49:07 -0600

    Create more meaningful unit tests for storybook-common components.

--- libs/storybook-common/src/lib/do-dont/DoDont.spec.tsx
+++ libs/storybook-common/src/lib/do-dont/DoDont.spec.tsx
@@ -3,9 +3,15 @@ import { render } from '@testing-library/react';
 
 import { DoDont } from './DoDont';
 
-describe(' DoDont', () => {
+describe('DoDont', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<DoDont dos={"Test do's"} donts={"Test dont's"} />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it(`should render both do's and dont's` , () => {
+    const { getByText } = render(<DoDont dos={"Test do's"} donts={"Test dont's"} />);
+    expect(getByText(`Test do's`)).toBeTruthy();
+    expect(getByText(`Test dont's`)).toBeTruthy();
   });
 });
