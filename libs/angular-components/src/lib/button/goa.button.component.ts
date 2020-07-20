471f842d738ebed4c1fbc8fc1cc9852c33d434c3
Paul <haipeng3@outlook.com>
2020-07-20 11:45:25 -0600

    Update the comments CS-73

--- libs/angular-components/src/lib/button/goa.button.component.ts
+++ libs/angular-components/src/lib/button/goa.button.component.ts
@@ -3,6 +3,21 @@ import { Button } from './button';
 import ButtonHtml from './goa.button.component.html'
 import ButtonStyle from './goa.button.component.css'
 
+/***
+Paul.Li July-20-2020
+
+Note: Ideally, we shall use 
+@Component({
+  selector: 'goa-button',
+  templateUrl: <html_path>,
+  styles: [<style_file_path>]
+})
+
+to include the html and style templates into the component.
+However, Paul faced a 404 issue (cannot template files) when tried to include them based on Url.
+Using 'template' and 'styles' to include templates is a temporarily workaround.
+***/
+
 @Component({
   moduleId: module.id,
   selector: 'goa-button',
