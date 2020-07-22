6d5e1c67b4472e507553d82cdcb2bf336712b7ae
David Kraushar <david.kraushar@gmail.com>
2020-07-22 08:09:07 -0600

    Add addons for angular storybook, get sub storybook working for css.

--- libs/angular-components/.storybook/main.js
+++ libs/angular-components/.storybook/main.js
@@ -2,6 +2,11 @@ module.exports = {
     stories: ['../src/lib/**/*.stories.mdx'],
     addons: [
       '@storybook/addon-docs/preset',
-      '@storybook/addon-knobs'
+      '@storybook/addon-knobs',
+      '@storybook/addon-backgrounds',
+      '@storybook/addon-a11y/register',
+      '@storybook/addon-viewport',
+      'storybook-addon-xd-designs',
+      '@storybook/addon-knobs/register',
     ]
 };
