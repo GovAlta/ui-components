4e1e6ddc6bbbf130eea4ccf5c65515e0e24312d7
David Kraushar <david.kraushar@gmail.com>
2020-07-23 10:33:01 -0600

    Add unit tests for button.

---
+++ libs/angular-components/src/lib/button/button.component.spec.ts
@@ -0,0 +1,45 @@
+import { render, screen, fireEvent } from '@testing-library/angular';
+import { GoAButtonComponent } from './button.component';
+
+describe('Goa Button', () => {
+  test('should render title', async () => {
+    await render(GoAButtonComponent, {
+      componentProperties: { title: 'Test Title' },
+    });
+
+    expect(screen.getByText('Test Title'))
+  });
+
+  test('buttonType=primary should render primary styling', async () => {
+    await render(GoAButtonComponent, {
+      componentProperties: { buttonType: 'primary', title: 'Test Title' },
+    });  
+    
+    const button = screen.getByText('Test Title');
+    expect(button.className).toContain('goa-button');
+    expect(button.className).not.toContain('goa-button--secondary');
+    expect(button.className).not.toContain('goa-button--tertiary');
+  });
+
+  test('buttonType=secondary should render secondary styling', async () => {
+    await render(GoAButtonComponent, {
+      componentProperties: { buttonType: 'secondary', title: 'Test Title' },
+    });   
+
+    const button = screen.getByText('Test Title');
+    expect(button.className).toContain('goa-button');
+    expect(button.className).toContain('goa--secondary');
+    expect(button.className).not.toContain('goa--tertiary');
+  });
+
+  test('buttonType=tertiary should render tertiary styling', async () => {
+    await render(GoAButtonComponent, {
+      componentProperties: { buttonType: 'tertiary', title: 'Test Title' },
+    });   
+
+    const button = screen.getByText('Test Title');
+    expect(button.className).toContain('goa-button');
+    expect(button.className).not.toContain('goa--secondary');
+    expect(button.className).toContain('goa--tertiary');
+  });
+});
