a9165e7ed14f8705e6824bfa61d6a06e53823855
David.Kraushar <david.kraushar@gov.ab.ca>
2020-07-27 12:59:43 -0600

    Get css exporting as asset to core-css project. Get js module exporting to core-css. Usage added to readme.

---
+++ samples/angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'test-app';
+
+  clickHandler() {
+    console.log('I was clicked!');
+  }
+}
