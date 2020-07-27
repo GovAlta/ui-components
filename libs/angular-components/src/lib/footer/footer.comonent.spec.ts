aeb2795da0e20ad2680ada0aa5b7139e46935548
Paul <haipeng3@outlook.com>
2020-07-27 11:33:26 -0600

    Update the footer testing CS-60

--- libs/angular-components/src/lib/footer/footer.comonent.spec.ts
+++ libs/angular-components/src/lib/footer/footer.comonent.spec.ts
@@ -1,16 +1,21 @@
 import { render, screen, fireEvent } from '@testing-library/angular';
 import { GoAFooterComponent } from './footer.component';
 
-describe('Goa Button', () => {
+describe('GoA footer', () => {
   const serviceName = 'DIO service';
 
-  test('should render f', async () => {
+  test('should render footer', async () => {
     await render(GoAFooterComponent, {
       componentProperties: { serviceName: serviceName },
     });
 
+    const footerLogo = screen.findByTestId('goa-footer-logo');
+    const footerTopBar = screen.findByTestId('goa-footer-top-bar');
+
     expect(screen.getByText(serviceName))
     expect(screen.getByText('An official site of the'))
+    expect((await footerLogo).className).toContain('logo')
+    expect((await footerTopBar).className).toContain('top-bar')
   });
 
 });
