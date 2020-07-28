e12183274c62b01075967f35488273cf72a040a8
Adrian Wilson <wilson.adrian@gmail.com>
2020-07-28 11:33:34 -0600

    vue  schematics/builder

--- libs/vue-plugin/src/schematics/vue-plugin/schema.d.ts
+++ libs/vue-plugin/src/schematics/vue-plugin/schema.d.ts
@@ -2,4 +2,9 @@ export interface VuePluginSchematicSchema {
   name: string;
   tags?: string;
   directory?: string;
+  style: 'css' | 'scss' | 'less' | 'stylus';
+  unitTestRunner: 'jest' | 'none';
+  e2eTestRunner: 'cypress' | 'none';
+  routing: boolean;
+  skipFormat: boolean;
 }
