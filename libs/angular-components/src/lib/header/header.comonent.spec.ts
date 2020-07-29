1a2aaa1ce754a8634b5e21187e4da7c13100b51c
David Kraushar <david.kraushar@gmail.com>
2020-07-29 14:32:02 -0600

    Update tests to split out @Inputs into different tests.

--- libs/angular-components/src/lib/header/header.comonent.spec.ts
+++ libs/angular-components/src/lib/header/header.comonent.spec.ts
@@ -12,8 +12,14 @@ describe('GoA Header', () => {
       declarations: [GoAMicrositeLogoComponent]
     });
 
-    expect((<HTMLAnchorElement>screen.getByRole('link', { name: microSiteLink })).href).toEqual(microSiteLink);
     expect(screen.getByText(serviceName));
   });
 
+  test('should link to the microServiceHomeLink', async () => {
+    await render(GoAMicrositeLogoComponent, {
+      componentProperties: { serviceName: serviceName, microServiceHomeLink: microSiteLink }
+    });
+    
+    expect((<HTMLAnchorElement>screen.getByRole('link', { name: microSiteLink })).href).toEqual(microSiteLink);
+  });
 });
