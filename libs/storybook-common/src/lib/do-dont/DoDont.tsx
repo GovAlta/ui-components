688bf9531031ea896b946a38303dbba422839bb7
Adrian Wilson <wilson.adrian@gmail.com>
2020-08-05 15:36:36 -0600

    test checkin

--- libs/storybook-common/src/lib/do-dont/DoDont.tsx
+++ libs/storybook-common/src/lib/do-dont/DoDont.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Callout } from '../callout/Callout';
-
+const i = 0;
 /**
  * Show a list Do's and Don'ts in a callout format.
  * @property {string} dos The "do's content text"
  * @property {string} donts The "dont's content text"
  */
-export const DoDont = ({dos, donts}) => {
+export const DoDont = ({ dos, donts }) => {
   return (
     <div>
       <Callout positive={true} content={dos} />
       <Callout positive={false} content={donts} />
     </div>
-  )
-}
+  );
+};
