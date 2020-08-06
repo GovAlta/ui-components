0b37072683e0aff194f4b04dedae5eb1886a51bc
David Kraushar <david.kraushar@gmail.com>
2020-08-06 10:42:02 -0600

    Add storybook and storybook-build commands.

--- libs/vue-components/.storybook/main.js
+++ libs/vue-components/.storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
     stories: ['../src/lib/**/*.stories.mdx', '../../storybook-common/src/lib/**/*.stories.mdx'],
     addons: [
       '@storybook/addon-docs/preset',
+      '@storybook/preset-scss',
       '@storybook/addon-knobs',
       '@storybook/addon-backgrounds',
       '@storybook/addon-a11y/register',
