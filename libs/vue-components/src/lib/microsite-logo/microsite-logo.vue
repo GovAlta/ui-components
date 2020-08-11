1a9aa6ac68d0b91d092abc40f2593a8968168079
David.Kraushar <david.kraushar@gov.ab.ca>
2020-08-11 13:45:32 -0600

    Revert "Make common a module."
    This reverts commit d2a8bdfc60ac17eeac79adc1cf1475d82d697690

---
+++ libs/vue-components/src/lib/microsite-logo/microsite-logo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="goa-microsite-logo">
+        <div class="logo">
+            <a :href="microServiceHomeLink" :aria-label="microServiceHomeLink" class='microsite-link'>
+                <img class="image-desktop-tablet" alt="Government of Alberta"
+                   :src="DesktopLogo" />
+                <img class="image-mobile" alt="Government of Alberta" :src="MobileLogo" />
+                <span class="name">
+                    {{serviceName}}
+                </span>
+            </a>
+        </div>
+    </div>
+</template>
+<script lang="typescript">
+    import { DesktopLogo, MobileLogo } from '@abgov/shared/common';
+
+    export default {
+        name: 'goa-microsite-logo',
+        props: {
+            'microServiceHomeLink': {
+                type: String,
+                required: true
+            },
+            'serviceName': {
+                type: String,
+                required: true
+            }
+        },
+        data () {
+            return {
+                DesktopLogo: DesktopLogo,
+                MobileLogo: MobileLogo
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    @import '../../../../core-css/src/lib/microsite-logo/microsite-logo.scss';
+</style>
---
+++ samples/vue/README.md
@@ -0,0 +1,14 @@
+# Vue Sample Usage
+
+This project was created to show simple consumption of the @abgov/vue-components library.  
+A full listing of available components and further documentation can be found at: https://ui-components-dio-dev.os99.gov.ab.ca/vue/.
+
+## To build
+
+Install any missing NPM packages by running `npm install`.  
+Run `ng build` from the current directory.
+
+## To run
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:8080/`. The app will automatically reload if you change any of the source files.
+
