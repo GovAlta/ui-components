a34e56e94b74fecf0064ba003343aefa48157bde
Adrian Wilson <wilson.adrian@gmail.com>
2020-08-20 11:34:55 -0600

    ci: back to regular versioning

--- publish.js
+++ publish.js
@@ -2,26 +2,10 @@ const fs = require('fs'); // Or `import fs from "fs";` with ESM
 const { argv } = require('yargs');
 const path = argv.path;
 const { exec } = require('child_process');
-const util = require('util');
-const gitSemverTags = util.promisify(require('git-semver-tags'));
 
 if (fs.existsSync(path)) {
-  deployGit(path);
-}
-
-console.log(`${path} not executed`);
-
-async function deployGit(path) {
-  const tags = await gitSemverTags();
   const { exec } = require('child_process');
-  const nextVersion = tags[0];
-  const nextTagArray = nextVersion.split('.');
-  //next angular pattern: https://angular.io/guide/releases
-  //can version next releases (x-next) down the road.
-  nextTagArray.splice(2, 0, '0-next');
-  const script = `npm-deploy-git-tag --token ${
-    process.env.NPM_TOKEN
-  } --access public --token ${nextTagArray.join('.')}`;
+  const script = `npm-deploy-git-tag --token ${process.env.NPM_TOKEN} --access public`;
   console.log(`Executing: ${script}`);
   exec(script, { cwd: path }, (error, stdout, stderr) => {
     if (error) {
@@ -37,3 +21,5 @@ async function deployGit(path) {
 
   return;
 }
+
+console.log(`${path} not executed`);
