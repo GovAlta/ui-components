96b0aba15d6440f4ab1a19b92cf475af74f2ab91
Adrian Wilson <wilson.adrian@gmail.com>
2020-08-24 11:41:19 -0600

    ci: build fail test

--- libs/angular-components/src/lib/button/button.component.spec.ts
+++ libs/angular-components/src/lib/button/button.component.spec.ts
@@ -12,24 +12,24 @@ describe('GoA Button', () => {
       template: `<button data-testid='testid' goa-button buttonType='primary'>${buttonTitle}</button>`,
     });
 
-    expect(screen.getByText(buttonTitle))
+    expect(screen.getByText(buttonTitle));
   });
 
   test('buttonType=primary should render primary styling', async () => {
     await render(GoAButtonComponent, {
       template: `<button data-testid='testid' goa-button buttonType='primary'>${buttonTitle}</button>`,
     });
-    
+
     const button = screen.getByTestId('testid');
     expect(button.className).toContain(buttonClassName);
-    expect(button.className).not.toContain(buttonSecondaryClassName);
+    expect(button.className).toContain(buttonSecondaryClassName);
     expect(button.className).not.toContain(buttonTertiaryClassName);
   });
 
   test('buttonType=secondary should render secondary styling', async () => {
     await render(GoAButtonComponent, {
       template: `<button data-testid='testid' goa-button buttonType='secondary'>${buttonTitle}</button>`,
-    }); 
+    });
 
     const button = screen.getByTestId('testid');
     expect(button.className).toContain(buttonClassName);
