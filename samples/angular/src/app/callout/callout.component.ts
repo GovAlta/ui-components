1d09e9f1e0ee5d179644b641244e096884740ddb
David.Kraushar <david.kraushar@gov.ab.ca>
2020-09-15 10:15:10 -0600

    feat: Add callout component

---
+++ samples/angular/src/app/callout/callout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'callout-sample',
+  templateUrl: './callout.component.html',
+  styleUrls: ['./callout.component.css']
+})
+export class CalloutSampleComponent {
+
+}
