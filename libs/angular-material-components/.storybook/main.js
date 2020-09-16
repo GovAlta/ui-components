2020b1323b68546aca0d6cfcf6edf09ab18f9faa
Adrian.Wilson <adrian.wilson@gov.ab.ca>
2020-09-16 09:05:47 -0600

    feat: angular material theme with button styles as per ab design theme

---
+++ libs/angular-material-components/.storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+    stories: ['../src/lib/**/*.stories.mdx', '../../shared/storybook-common/src/lib/**/*.stories.mdx'],
+    addons: [
+      '@storybook/addon-docs/preset',
+      '@storybook/addon-backgrounds',
+      '@storybook/addon-controls', 
+      '@storybook/addon-actions',     
+      '@storybook/addon-a11y',
+      '@storybook/addon-viewport',
+      'storybook-addon-xd-designs'
+    ]
+};
