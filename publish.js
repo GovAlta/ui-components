51a6bfe4fd10a2beaa8ea7d02a2375dbc8ef3448
Adrian Wilson <wilson.adrian@gmail.com>
2020-09-22 11:07:17 -0600

    ci: improve dependency publishing for core packages

--- publish.js
+++ publish.js
@@ -1,26 +1,29 @@
-const fs = require('fs'); // Or `import fs from "fs";` with ESM
-const { argv } = require('yargs');
-const { path, dependency } = argv; //support for single dependency for now
-const { exec } = require('child_process');
+const fs = require('fs');
+const readPkg = require('read-pkg');
+const writePkg = require('write-pkg');
+const {
+  argv: { path, dependency },
+} = require('yargs'); //support for single dependency for now
+const { execSync } = require('child_process');
 
 if (fs.existsSync(path)) {
-  console.log(`Dependency: ${dependency}`);
-  const depScript = dependency
-    ? `npm i ${dependency} && rm -f package-lock.json &&`
-    : '';
-  const script = `${depScript} npm-deploy-git-tag --token ${process.env.NPM_TOKEN} --access public --tag next`;
+  if (dependency) {
+    const version = execSync(`npm view ${dependency} version`);
+    console.log(
+      `Adding dependency: ${dependency}, Version ${version} to ${path}`
+    );
+    const packageMeta = readPkg.sync({ cwd: path });
+    writePkg.sync(path, {
+      ...packageMeta,
+      dependencies: {
+        ...packageMeta.dependencies,
+        [dependency]: `${version}`.trim(),
+      },
+    });
+  }
+  const script = `npm-deploy-git-tag --token ${process.env.NPM_TOKEN} --access public --tag next`;
   console.log(`Executing: ${script}`);
-  exec(script, { cwd: path }, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      throw new Error(error.message);
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      throw new Error(stderr);
-    }
-    console.log(`stdout: ${stdout}`);
-  });
+  execSync(script, { cwd: path });
 
   return;
 }
