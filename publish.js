286ae7cc96e8a62dcc170981ef91649999892f3c
Adrian.Wilson <adrian.wilson@gov.ab.ca>
2020-09-23 09:58:27 -0600

    fix: revert to manually installing core dependency

--- publish.js
+++ publish.js
@@ -2,25 +2,11 @@ const fs = require('fs');
 const readPkg = require('read-pkg');
 const writePkg = require('write-pkg');
 const {
-  argv: { path, dependency },
+  argv: { path },
 } = require('yargs'); //support for single dependency for now
 const { execSync } = require('child_process');
 
 if (fs.existsSync(path)) {
-  if (dependency) {
-    const version = execSync(`npm view ${dependency}@next version`);
-    console.log(
-      `Adding dependency: ${dependency}, Version ${version} to ${path}`
-    );
-    const packageMeta = readPkg.sync({ cwd: path });
-    writePkg.sync(path, {
-      ...packageMeta,
-      dependencies: {
-        ...packageMeta.dependencies,
-        [dependency]: `${version}`.trim(),
-      },
-    });
-  }
   const script = `npm-deploy-git-tag --token ${process.env.NPM_TOKEN} --access public --tag next`;
   console.log(`Executing: ${script}`);
   execSync(script, { cwd: path });
