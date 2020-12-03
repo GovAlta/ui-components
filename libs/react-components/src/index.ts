2afb1f2af9d16eab96ba9ccfa3e67d01e6811934
Chris.Olsen <chris.olsen@gov.ab.ca>
2020-12-03 08:59:55 -0700

    CS-203: add React radio button component

--- libs/react-components/src/index.ts
+++ libs/react-components/src/index.ts
@@ -3,6 +3,8 @@ import GoANotification from './lib/notification/notification';
 import GoAButton from './lib/button/button';
 import GoaHeader from './lib/header/header';
 import GoaMicrositeLogo from './lib/microsite-logo/microsite-logo';
-import GoaHeroBanner from './lib/hero-banner/hero-banner.component';
 
-export { GoACallout, GoANotification, GoAButton, GoaHeader, GoaMicrositeLogo, GoaHeroBanner };
+import GoaHeroBanner from './lib/hero-banner/hero-banner.component';
+import GoARadioGroup from './lib/radio-group/radio-group';
+
+export { GoANotification, GoARadioGroup, GoACallout, GoAButton, GoaHeader, GoaMicrositeLogo, GoaHeroBanner };
