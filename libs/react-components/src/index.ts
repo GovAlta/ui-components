d470ac592731346c6fd4c3d87caa1a30dd834189
athena.chen <athena.chen@gov.ab.ca>
2020-12-10 16:30:12 -0700

    forget put in index

--- libs/react-components/src/index.ts
+++ libs/react-components/src/index.ts
@@ -7,6 +7,7 @@ import GoaMicrositeLogo from './lib/microsite-logo/microsite-logo';
 import GoaHeroBanner from './lib/hero-banner/hero-banner.component';
 import GoACard from './lib/card/card.component';
 import GoARadioGroup from './lib/radio-group/radio-group';
+import GoACardGroup from './lib/card-group/card.group.component';
 
 export {
   GoANotification,
@@ -17,4 +18,5 @@ export {
   GoaMicrositeLogo,
   GoaHeroBanner,
   GoACard,
+  GoACardGroup
 };
