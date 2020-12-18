a64521f656f39f2adf302d3b53ea7eb3de35721e
athena.chen <athena.chen@gov.ab.ca>
2020-12-18 16:49:16 -0700

    Add callout vue component

---
+++ libs/vue-components/src/lib/callout/callout.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div :class=[calloutTypeClass]>
+      <div>
+        <h3>{{ title }}</h3>
+        <div class="messages">
+          {{ content }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="typescript">
+
+export default {
+  name: 'goa-callout',
+  props: {
+    /**
+     * Callout title.
+     */
+    title: {
+      type: String,
+      required: true,
+    },
+  /**
+   * The type of the callout, changes stylings and icons.
+  */
+    calloutType:{
+      type: String,
+      validator: (prop) => [
+      'important', 'information' , 'event' , 'emergency'
+      ].includes(prop)
+    },
+    /**
+     * Callout description
+     */
+    content: {
+      type: String,
+      required: false,
+    },
+  },
+  data() {
+    return {   
+        calloutTypeClass: `goa-callout goa--${this.calloutType}`
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import '../../../../core-css/src/lib/styles/callout/callout.scss';
+</style>
