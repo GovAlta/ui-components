2f22794aa5cf2eff5eb7317110849b6b626e9dc8
Chris Olsen <chris.olsen@gov.ab.ca>
2020-12-18 15:15:42 -0700

    clean vue components

--- libs/vue-components/.eslintrc.js
+++ libs/vue-components/.eslintrc.js
@@ -15,5 +15,14 @@ module.exports = {
       },
     },
   ],
-  ignorePatterns: ['!**/*']
+  ignorePatterns: ['!**/*'],
+  rules: {
+    "vue/max-attributes-per-line": ["error", {
+        "singleline": 3,
+        "multiline": {
+          "max": 1,
+          "allowFirstLine": false
+        }
+    }]
+  }
 };
