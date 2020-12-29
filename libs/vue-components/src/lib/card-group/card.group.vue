b0ec52759e7a0113eb4f4732be641924e829559d
athena.chen <athena.chen@gov.ab.ca>
2020-12-29 14:04:54 -0700

    add group

---
+++ libs/vue-components/src/lib/card-group/card.group.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="goa-card" :style="{ width: this.getCardWidth }">
+    <div class="goa-poster">
+      <img v-if="this.isImage" :src="this.cardImageUrl" alt="Card cardImageUrl" />
+    </div>
+
+    <div class="card-content">
+      
+       <a class="goa-title" v-if="this.isTitleUrl" :href="this.titleUrl"> {{ title }} </a>
+      <div class="goa-title" v-if="!this.isTitleUrl">{{ title }} </div>
+      
+      <div class="goa-text">
+        {{ description }}
+      </div>
+       <div  slot-scope class="goa-footer">
+         <slot/>
+       </div>
+    </div>
+  </div>
+</template>
+<script lang="typescript">
+export default {
+  name: 'goa-card',
+  props: {
+    /**
+     * Card title.
+     */
+    title: {
+      type: String,
+      required: true,
+    },
+        /**
+     * Card title url .
+     */
+    titleUrl: {
+      type: String,
+      required: false,
+    },
+
+    /**
+     * Card description
+     */
+    description: {
+      type: String,
+      required: false,
+    },
+    /** Card image , display on top of title */
+    cardImageUrl: {
+      type: String,
+      required: false,
+    },
+    /** Display card size to allow card responsively show in different device*/
+    cardWidth: {
+       type: [ String, Number],
+       required: false,
+       default:'auto',
+
+    },
+  },
+  data() {
+    return {
+      
+    };
+  },
+  computed: {
+    isImage() {
+      return Boolean(this.cardImageUrl);
+    },
+    isTitleUrl() {
+      return Boolean(this.titleUrl);
+    },
+
+    getCardWidth(){
+      if((typeof this.cardWidth) === "number"){
+        return this.cardWidth+"px";
+      }
+     return (this.cardWidth==='full'? this.cardWidth:'auto');
+    }
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import '../../../../core-css/src/lib/styles/card-group/card-group.scss';
+</style>
