e6af1f6f2bde68b285e19374de982d6c77d2b54e
Ting Zuge <ting.zuge@gov.ab.ca>
2021-01-14 14:38:27 -0700

    Enabling composition on the 'core' storybook. Reordering nginx locations to try to fix path without trailing slash (ex /angular) not working.

--- libs/core-css/.storybook/main.js
+++ libs/core-css/.storybook/main.js
@@ -1,17 +1,35 @@
 module.exports = {
-    stories: ['../src/lib/**/*.stories.mdx', '../../shared/storybook-common/src/lib/**/*.stories.mdx'],
-    addons: [
-      {
-        name: '@storybook/addon-docs',
-        options: {
-        }
-      },
-      {
-        name: '@storybook/preset-scss'
-      },
-      '@storybook/addon-backgrounds',
-      '@storybook/addon-a11y',
-      '@storybook/addon-viewport',
-      'storybook-addon-xd-designs'
-    ]
+  stories: ['../src/lib/**/*.stories.mdx', '../../shared/storybook-common/src/lib/**/*.stories.mdx'],
+  addons: [
+    {
+      name: '@storybook/addon-docs',
+      options: {
+      }
+    },
+    {
+      name: '@storybook/preset-scss'
+    },
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport',
+    'storybook-addon-xd-designs'
+  ],
+  refs: {
+    angular: { 
+      title: "Angular", 
+      url: '/angular/'
+    },
+    angularMat: { 
+      title: "Angular Material", 
+      url: '/angular-material/'
+    },
+    react: { 
+      title: "React", 
+      url: '/react/'
+    },
+    vue: { 
+      title: "Vue", 
+      url: '/vue/'
+    }
+  }
 };
