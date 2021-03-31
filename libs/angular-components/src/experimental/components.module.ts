6c20cf4b2c9558afb45c29724400878568a07110
Taryn.Laramie <taryn.laramie@gov.ab.ca>
2021-03-31 15:42:22 -0600

    Experimental app version header feature

--- libs/angular-components/src/experimental/components.module.ts
+++ libs/angular-components/src/experimental/components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoABadgeComponent } from './badge/badge.component';
+import { GoAAppVersionHeaderComponent } from './app-version-header/app-version-header.component';
 
 @NgModule({
   imports: [CommonModule],
   exports: [
-    GoABadgeComponent
+    GoABadgeComponent,
+    GoAAppVersionHeaderComponent
   ],
   declarations: [
-    GoABadgeComponent
+    GoABadgeComponent,
+    GoAAppVersionHeaderComponent
   ],
   providers: [
   ],
 })
-export class ExperimentalComponentsModule {} 
+export class ExperimentalComponentsModule { }
