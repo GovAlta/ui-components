d587c5c51ee9a11994655700a154b40d399c8c99
Chris Olsen <chris.olsen@gov.ab.ca>
2021-06-07 16:09:46 -0600

    export the GoARadio component

--- libs/react-components/src/index.ts
+++ libs/react-components/src/index.ts
@@ -8,7 +8,7 @@ import GoAHeroBanner from './lib/hero-banner/hero-banner.component';
 import GoAHeroBannerContent from './lib/hero-banner/content/hero-banner-content.component';
 import GoAHeroBannerLink from './lib/hero-banner/link/hero-banner-link.component';
 import GoACard from './lib/card/card.component';
-import GoARadioGroup from './lib/radio-group/radio-group';
+import { GoARadioGroup, GoARadio } from './lib/radio-group/radio-group';
 import GoACardGroup from './lib/card-group/card.group.component';
 import { GoADropdown, GoAOption, GoAOptionGroup } from './lib/dropdown/dropdown.component';
 import GoACheckbox from './lib/checkbox/checkbox';
@@ -16,6 +16,7 @@ import GoACheckbox from './lib/checkbox/checkbox';
 export {
   GoANotification,
   GoARadioGroup,
+  GoARadio,
   GoACallout,
   GoAButton,
   GoAHeader,
