7534768673a0e791f2b027ceb174e297a3f3a897
Ting Zuge <ting.zuge@gov.ab.ca>
2021-06-18 06:46:47 -0600

    correcting issue for build of angular-components.

--- libs/angular-components/src/experimental/index.ts
+++ libs/angular-components/src/experimental/index.ts
@@ -1,4 +1,5 @@
 export * from './badge/badge.component';
 export * from './app-version-header/app-version-header.component';
 export * from './page-load-indicator/page-load-indicator.component';
+export * from './element-load-indicator/element-load-indicator.component';
 export * from './components.module';
