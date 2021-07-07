dd45844b3c6f13e473e2fdb14dc5174a8688a186
Kevin.L.Gauld <kevin.l.gauld@gov.ab.ca>
2021-07-07 00:14:57 +0000

    React Skeleton Element - CS-432

---
+++ libs/react-components/src/experimental/skeleton/skeleton-shimmer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './skeleton-element.scss';
+
+export const GoAShimmer = () => {
+  return (
+    <div className="shimmer-wrapper">
+      <div className="shimmer"></div>
+    </div>
+  )
+}
---
+++ libs/react-components/src/experimental/skeleton/skeleton-titled-content.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { GoASkeletonElement } from './skeleton-element';
+import { GoAShimmer } from './skeleton-shimmer';
+import './skeleton-element.scss';
+
+export interface DisplayProps {
+  /**
+   * The number of rows to display.
+   */
+  rows?: number;
+}
+
+export const GoASkeletonContent = ({ rows = 1 }: DisplayProps) => {
+  const elements = [];
+
+  for (let i = 0; i < rows; i++) {
+    elements.push(<GoASkeletonElement type="text" />);
+  }
+
+  return (
+    <div className="skeleton-wrapper">
+      <div className="skeleton-content">
+        <div>
+          <GoASkeletonElement type="title" />
+
+          {elements}
+        </div>
+      </div>
+      <GoAShimmer />
+    </div>
+  );
+};
