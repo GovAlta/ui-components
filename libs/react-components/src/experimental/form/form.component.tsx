ffd6f9cdc0ca0df9751b58f84665b5045a2234e8
athena.chen <athena.chen@gov.ab.ca>
2021-07-06 20:02:58 -0600

    move submit out of storybook

--- libs/react-components/src/experimental/form/form.component.tsx
+++ libs/react-components/src/experimental/form/form.component.tsx
@@ -1,7 +1,9 @@
 import React, { FormEvent, FC, ReactNode, useState } from 'react';
 import { GoAFormItem } from './form.item/form.item.component';
 import { GoAFormButton } from './form.item/form.button.component';
+import GoACallout from '../../lib/callout/callout';
 import './form.scss';
+import { GoAButton } from '../../lib/button/button';
 
 interface Props {
   onSubmit?: (e: FormEvent<Element>) => void;
@@ -25,7 +27,26 @@ export const GoAForm: FC<Props> = ({ onSubmit = null, children = null }) => {
       {onSubmit ? (
         <form onSubmit={handleOnSubmit} >
           {children}
-
+          <GoACallout
+            type='emergency'
+            title='Please fix the following errors'>
+            <ul>
+              <li>Coffee</li>
+              <li>Tea</li>
+              <li>Milk</li>
+            </ul>
+          </GoACallout>
+          <GoAFormButton>
+            <GoAButton buttonType="tertiary" type="button">
+              Cancel
+        </GoAButton>
+            <GoAButton
+              buttonType="primary"
+              type="submit"
+            >
+              Submit
+        </GoAButton>
+          </GoAFormButton>
         </form>
       ) : (
           children
