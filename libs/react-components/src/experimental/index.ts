484bf9717d5b14e43864f1805f2ba66cc1c02271
athena.chen <athena.chen@gov.ab.ca>
2021-07-16 09:42:02 -0600

    export formComponent

--- libs/react-components/src/experimental/index.ts
+++ libs/react-components/src/experimental/index.ts
@@ -1,5 +1,5 @@
 import { GoABadge } from './badge/badge.component';
 import { GoAPageLoader } from './page-loader/page-loader';
 import { GoAElementLoader } from './element-loader/element-loader';
-
-export { GoABadge, GoAPageLoader, GoAElementLoader };
+import { GoAFormItem, GoAFormButton, GoAFormContainer, GoAForm } from './form/form.component';
+export { GoABadge, GoAPageLoader, GoAElementLoader, GoAFormItem, GoAFormButton, GoAFormContainer, GoAForm };
