443a1584f96d6f088675ef653c29060893873c7f
Paul.Li <paul.li@gov.ab.ca>
2021-07-26 13:41:47 -0600

    Delete the expriment/index.ts

--- libs/react-components/src/index.ts
+++ libs/react-components/src/index.ts
@@ -16,20 +16,13 @@ import {
   GoAOptionGroup,
 } from './lib/dropdown/dropdown.component';
 import GoACheckbox from './lib/checkbox/checkbox';
-
-import {
-  GoABadge,
-  GoAPageLoader,
-  GoAElementLoader,
-  GoASkeletonContent,
-  GoASkeletonGridColumnContent,
-  GoASkeletonImageContent,
-  GoAFormItem,
-  GoAFormButton,
-  GoAFormContainer,
-  GoAForm
-} from './experimental'
-
+import { GoABadge } from './experimental/badge/badge.component';
+import { GoAPageLoader } from './experimental/page-loader/page-loader';
+import { GoAElementLoader } from './experimental/element-loader/element-loader';
+import { GoAFormItem, GoAFormButton, GoAFormContainer, GoAForm } from './experimental/form/form.component';
+import { GoASkeletonContent } from './experimental/skeleton/skeleton-titled-content';
+import { GoASkeletonGridColumnContent } from './experimental/skeleton/skeleton-grid-column';
+import { GoASkeletonImageContent } from './experimental/skeleton/skeleton-image-content';
 export {
   GoABadge,
   GoAPageLoader,
