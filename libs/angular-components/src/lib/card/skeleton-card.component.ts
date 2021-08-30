75638319bde4a06a0021a458632fb6d1cb4cd5ce
Kevin Gauld <kevin.gauld@loopspin.com>
2021-08-30 09:23:20 -0400

    Update card animation

--- libs/angular-components/src/lib/card/skeleton-card.component.ts
+++ libs/angular-components/src/lib/card/skeleton-card.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'goa-skeleton-card',
   templateUrl: './skeleton-card.component.html',
-  styleUrls: [],
+  styleUrls: ['./card.component.scss'],
 })
 export class GoALoadingCardComponent {
   constructor() {}
