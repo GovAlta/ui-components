cc7bd2af7212f37374fa7ae80786bdeaa8296b0a
Chris Olsen <chris.olsen@gov.ab.ca>
2021-09-09 12:23:37 -0600

    feat: add transition components to separate styling from functionality

---
+++ libs/react-components/src/experimental/transitions/transitionSequence.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react'
+import { GoATransition, GoATransitionProps } from './transition';
+
+interface Props {
+  transitions: string[];
+  transitionIndex: number;
+  onComplete?: (index: number, done: boolean) => void;
+}
+
+export const GoATransitionSequence: FC<Props> = (props) => {
+  function createTransition(props: GoATransitionProps, children?: JSX.Element): JSX.Element {
+    return <GoATransition {...props}>{ children }</GoATransition>
+  }
+
+  return props.transitions
+    // create components for the transitions
+    .map((state: string, index: number): JSX.Element => {
+      return createTransition({
+        active: (props.transitionIndex % (props.transitions.length + 1)) > index,
+        name: state,
+        onComplete: () => props?.onComplete(index, index === props.transitions.length - 1)
+      })
+    })
+    // on `reduce` we need to start with the innermost child
+    .reverse()
+    // create parent/child relationship between components
+    .reduce((child: JSX.Element, parent: JSX.Element): JSX.Element => {
+      if (!child) {
+        // the original children (user defined content to be transitioned)
+        return React.cloneElement(parent, {
+          children: props.children
+        })
+      }
+      return React.cloneElement(parent, {
+        children: child
+      })
+    }, null)
+}
