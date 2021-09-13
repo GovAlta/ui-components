121bc7446dad4dc0bdf3715d316c137998830bbd
Chris Olsen <chris.olsen@gov.ab.ca>
2021-09-13 09:15:20 -0600

    feat: add pencil (edit) icon

--- libs/react-components/src/experimental/icons/iconButton.component.tsx
+++ libs/react-components/src/experimental/icons/iconButton.component.tsx
@@ -7,12 +7,13 @@ interface Props extends TestProps {
   type: IconType,
   size?: IconSize;
   variant?: IconVariant;
+  title?: string;
   onClick: () => void;
 }
 
-export function GoAIconButton({ type, variant = 'goa', onClick, size = 'small', testId }: Props & TestProps): JSX.Element {
+export function GoAIconButton({ type, variant = 'goa', onClick, size = 'small', title, testId }: Props & TestProps): JSX.Element {
   return (
-    <div className={`goa-icon-button goa-icon-button-${variant}`} data-testid={testId} onClick={onClick}>
+    <div title={title} className={`goa-icon-button goa-icon-button-${variant}`} data-testid={testId} onClick={onClick}>
       <GoAIcon type={type} size={size}></GoAIcon>
     </div>
   )
