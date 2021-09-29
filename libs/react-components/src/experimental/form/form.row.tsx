f1ce3e89d6c63b61bb2e3d0736e96df0a7cdcfb5
Chris Olsen <chris.olsen@gov.ab.ca>
2021-09-29 16:06:34 -0600

    feat: form row component

---
+++ libs/react-components/src/experimental/form/form.row.tsx
@@ -0,0 +1,9 @@
+import React, { CSSProperties, FC } from 'react'
+
+export const GoAFormRow: FC = ({ children }) => {
+  return (
+    <div className="goa-form-row">
+      {children}
+    </div>
+  )
+}
