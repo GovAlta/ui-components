0537c976bef0f3a74ec903eb4ac1aadfd246ef6a
Kevin Gauld <kevin.gauld@loopspin.com>
2021-10-05 21:37:54 -0400

    feat: Add vue skeleton elements

---
+++ libs/vue-components/src/lib/skeleton/skeleton-loading-card-no-image.vue
@@ -0,0 +1,27 @@
+<template>
+  <goa-card
+    data-skeleton
+    title="Lorem ipsum dolor sit amet elit"
+    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+  >
+    Lorem ipsum dolor sit amet
+  </goa-card>
+</template>
+
+<script lang="typescript">
+
+import GoACard from '../card/card.vue';
+
+export default {
+  name: 'skeleton-loading-no-image',
+  components: {
+    'goa-card': GoACard,
+  },
+  props: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './skeleton-element.scss';
+</style>
