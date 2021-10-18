2625f42897f988f44da902af140d137253592785
Chris Olsen <chris.olsen@gov.ab.ca>
2021-10-18 09:34:35 -0600

    feat: add explicit input GoA components

--- libs/react-components/src/experimental/index.ts
+++ libs/react-components/src/experimental/index.ts
@@ -4,7 +4,10 @@ import { GoACard } from './card/card.component';
 import { GoAFlexRow } from './flex';
 import { GoAFormItem, GoAFormActions, GoAForm } from './form/form.component';
 import { GoAIcon, GoAIconButton } from './icons';
-import { GoAInput } from './input/input.component';
+import {
+  GoAInput,
+  GoAInputText, GoAInputPassword, GoAInputDate, GoAInputTime, GoAInputDateTime, GoAInputEmail, GoAInputSearch, GoAInputUrl, GoAInputTel, GoAInputFile, GoAInputMonth, GoAInputNumber, GoAInputRange
+} from './input/input.component';
 import { GoAModal, GoAModalActions, GoAModalContent, GoAModalTitle } from './modal/modal.component';
 import { GoAScrollable } from './scrollable/scrollable.component';
 import { GoATextArea } from './textarea/textarea.component';
@@ -26,6 +29,19 @@ export {
   GoAIcon,
   GoAIconButton,
   GoAInput,
+  GoAInputText,
+  GoAInputPassword,
+  GoAInputDate,
+  GoAInputTime,
+  GoAInputDateTime,
+  GoAInputEmail,
+  GoAInputSearch,
+  GoAInputUrl,
+  GoAInputTel,
+  GoAInputFile,
+  GoAInputMonth,
+  GoAInputNumber,
+  GoAInputRange,
   GoAModal,
   GoAModalActions,
   GoAModalContent,
