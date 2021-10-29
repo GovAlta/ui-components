f97c4962b105b597bfe6c18029b9b9775e522173
Chris Olsen <chris.olsen@gov.ab.ca>
2021-10-29 16:20:57 -0600

    style: improve GoAInput styling

--- libs/react-components/src/experimental/input/input.component.tsx
+++ libs/react-components/src/experimental/input/input.component.tsx
@@ -1,20 +1,46 @@
-import React, { FC } from 'react';
-import { GoAIcon, GoAIconType } from '../icons';
+import React, { FC, MutableRefObject, useEffect, useState } from 'react';
+import { GoAIcon, GoAIconButton, GoAIconType } from '../icons';
 import { OnChange } from '../../lib/_common/input';
 import './input.scss';
+import classNames from 'classnames';
 
 interface Props {
+  // required
   name: string;
   value: string;
+  onChange: OnChange;
 
+  // optional
   id?: string;
   disabled?: boolean;
   placeholder?: string;
   leadingIcon?: GoAIconType;
-  onChange: OnChange;
+  trailingIcon?: GoAIconType;
+  onTrailingIconClick?: () => void;
+  variant?: 'goa' | 'bare';
+  focused?: boolean;
 };
 
-export const GoAInput: FC<Props & { type: string }> = ({ onChange, leadingIcon, ...props }) => {
+export const GoAInput: FC<Props & { type: string }> = ({
+  name,
+  onTrailingIconClick,
+  onChange,
+  leadingIcon,
+  trailingIcon,
+  variant = 'goa',
+  focused,
+  ...other
+}) => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (focused) {
+      inputRef.current?.focus();
+    } else {
+      inputRef.current?.blur();
+    }
+  }, [focused, inputRef]);
+
   return (
     <div className="goa-input">
       {leadingIcon &&
@@ -23,10 +49,20 @@ export const GoAInput: FC<Props & { type: string }> = ({ onChange, leadingIcon, 
         </div>
       }
       <input
-        className={leadingIcon ? 'input--leading-icon' : ''}
-        {...props}
-        onChange={(e) => onChange(props.name, e.target.value)}
+        ref={inputRef}
+        className={classNames({
+          [`input--${variant}`]: true,
+          'input--leading-icon': leadingIcon,
+        })}
+        {...other}
+        onChange={(e) => onChange(name, e.target.value)}
       />
+      {trailingIcon &&
+        <div onClick={() => onTrailingIconClick?.()} className="goa-input-trailing-icon">
+          {!onTrailingIconClick && <GoAIcon size="medium" type={trailingIcon} />}
+          {onTrailingIconClick && <GoAIconButton variant="secondary" onClick={() => onTrailingIconClick?.()} size="medium" type={trailingIcon} />}
+        </div>
+      }
     </div>
   );
 };
@@ -40,7 +76,7 @@ export const GoAInputPassword: FC<Props> = (props) => {
 }
 
 export const GoAInputDate: FC<Props & { min?: string, max?: string }> = (props) => {
-  return <GoAInput {...props} type="date" />;
+  return <GoAInput {...props} type="date" trailingIcon="calendar" />;
 }
 
 export const GoAInputTime: FC<Props> = (props) => {
@@ -48,7 +84,7 @@ export const GoAInputTime: FC<Props> = (props) => {
 }
 
 export const GoAInputDateTime: FC<Props & { min?: string, max?: string }> = (props) => {
-  return <GoAInput {...props} type="datetime-local" />;
+  return <GoAInput {...props} type="datetime-local" trailingIcon="calendar" />;
 }
 
 export const GoAInputEmail: FC<Props> = (props) => {
@@ -56,7 +92,7 @@ export const GoAInputEmail: FC<Props> = (props) => {
 }
 
 export const GoAInputSearch: FC<Props> = (props) => {
-  return <GoAInput {...props} type="search" />;
+  return <GoAInput {...props} type="text" trailingIcon="search" />;
 }
 
 export const GoAInputUrl: FC<Props> = (props) => {
@@ -68,19 +104,19 @@ export const GoAInputTel: FC<Props> = (props) => {
 }
 
 export const GoAInputFile: FC<Props> = (props) => {
-  return <GoAInput {...props} type="file" />;
+  return <input id={props.id} name={props.name} type="file" onChange={(e) => props.onChange(e.target.name, e.target.value)} style={{ backgroundColor: 'revert' }} />;
 }
 
 export const GoAInputMonth: FC<Props> = (props) => {
-  return <GoAInput {...props} type="month" />;
+  return <GoAInput {...props} type="month" trailingIcon="calendar" />;
 }
 
 export const GoAInputNumber: FC<Props & { min?: number, max?: number, step?: number }> = (props) => {
   return <GoAInput {...props} type="number" />;
 }
 
-export const GoAInputRange: FC<Props & { min?: number, max?: number, step?: number }> = (props) => {
-  return <GoAInput {...props} type="range" />;
+export const GoAInputRange: FC<Props & { min?: number, max?: number, step?: number }> = ({ step = 1, ...props }) => {
+  return <input id={props.id} min={props.min} max={props.max} step={step} name={props.name} type="range" onChange={(e) => props.onChange(e.target.name, e.target.value)} />;
 }
 
 export default GoAInput;
