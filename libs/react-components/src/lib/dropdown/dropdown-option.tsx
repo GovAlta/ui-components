66d3158ad68032b241651fe0ec1e86e561ce3dcf
Chris Olsen <chris.olsen@gov.ab.ca>
2021-11-23 13:36:19 -0700

    chore: move experimental dropdown to lib

---
+++ libs/react-components/src/lib/dropdown/dropdown-option.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from "react";
+import 'goa-web-components';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface IntrinsicElements {
+      'goa-dropdown-item': DropdownOptionProps & React.HTMLAttributes<HTMLElement>
+    }
+  }
+}
+
+interface DropdownOptionProps {
+  name: string;
+  value: string;
+  label: string;
+}
+
+interface Props {
+  name: string;
+  value: string;
+  label?: string;
+}
+
+export const GoADropdownOption: FC<Props> = (props) => {
+  return (
+    <goa-dropdown-item name={props.name} value={props.value} label={props.label}>{props.children}</goa-dropdown-item>
+  );
+}
+
+export default GoADropdownOption;
