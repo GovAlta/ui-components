38057bf6bd829ef362dd6301444f1f1c1bd2b0f7
Chris Olsen <olsen.chris@gmail.com>
2021-11-29 17:50:03 -0700

    feat: add container component

--- libs/web-components/demo/src/App.svelte
+++ libs/web-components/demo/src/App.svelte
@@ -5,12 +5,59 @@
 <main>
   <h1>Component Playground</h1>
 
+  <!-- Container -->
+  <goa-container headingsize="large">
+    <div slot="title">Header</div>
+    <goa-button type="tertiary" size="small" slot="actions">Button</goa-button
+    >
+    <p style="line-height: 1.6rem;">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  </goa-container>
+
+  <goa-container variant="primary" headingsize="large">
+    <div slot="title">Header</div>
+    <div slot="actions">
+      <goa-icon inverted type="timer" />
+      <goa-icon-button inverted type="alarm" />
+    </div>
+    <div>This is the container content</div>
+  </goa-container>
+
+  <goa-container variant="primary" headingsize="small">
+    <div slot="title">Header</div>
+    <div>This is the container content</div>
+  </goa-container>
+
+  <goa-container variant="error">
+    <div>This is the container content</div>
+  </goa-container>
+
+  <goa-container variant="success">
+    <div>This is the container content</div>
+  </goa-container>
+
+  <goa-container headingsize="none">
+    <div>This one has no header</div>
+  </goa-container>
+
+  <goa-container>
+    <h2>Interactive Container</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <goa-button type="borderless" size="large">Assign to me</goa-button>
+  </goa-container>
   <!-- Dropdown -->
 
   <h2>Dropdown</h2>
 
   <h3>Autocomplete and Custom option content</h3>
-  <goa-dropdown name="people" isautocomplete="autocomplete">
+  <goa-dropdown name="people">
     <goa-dropdown-item name="people" value="1" label="Tom Cruise" />
     <goa-dropdown-item name="people" value="2" label="Nicholas Cage">
       <div class="custom-dropdown-content">
