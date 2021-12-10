4d8b2c86d77fe5d213a524e2559ca36b81d6c160
Chris Olsen <olsen.chris@gmail.com>
2021-12-10 09:21:07 -0700

    add skeleton sizing

--- libs/web-components-orig/src/Skeleton.wc.svelte
+++ libs/web-components-orig/src/Skeleton.wc.svelte
@@ -2,32 +2,33 @@
 
 <!-- Script -->
 <script lang="ts">
-  export let type: "text" | "text-small" | "avatar" | "title" | "paragraph" | "thumbnail" | "card" | "profile";
+  export let size: number = 1;
+  export let type: "image" | "text" | "title" | "text-small" | "avatar" | "header" | "paragraph" | "thumbnail" | "card" | "profile";
 </script>
 
 <!-- HTML -->
 {#if type === "card"}
-  <div class="card">
-    <div class="card-image"></div>
+  <div class="card card-{size}">
+    <svelte:self type="image" {size} />
     <div class="card-content">
-      <svelte:self type="title" />
-      <svelte:self type="text" />
-      <svelte:self type="text" />
-      <svelte:self type="text" />
+      <svelte:self type="header" {size} />
+      <svelte:self type="text" {size} />
+      <svelte:self type="text" {size} />
+      <svelte:self type="text" {size} />
     </div>
   </div>
 {:else if type === "profile"}
-  <div class="profile">
+  <div class="profile {`profile-${size}`}">
     <div class="profile-avatar">
-      <svelte:self type="avatar" />
+      <svelte:self type="avatar" {size} />
     </div>
     <div class="profile-name">
-      <svelte:self type="text" />
-      <svelte:self type="text-small" />
+      <svelte:self type="title" {size} />
+      <svelte:self type="text-small" {size} />
     </div>
   </div>
 {:else}
-  <div class="skeleton {type}"></div>
+  <div class="skeleton {type} {`${type}-${size}`}"></div>
 {/if}
 
 <!-- Style -->
@@ -51,16 +52,67 @@
     margin: 10px 0;
   }
 
-  .text {
-    width: 100%;
-    height: 12px;
-    border-radius: 4px;
+  .image,
+  .image-1 {
+    background-color: var(--color-gray-100);
+    flex: 1 1 100px;
+    height: 100px;
+    margin: 0;
+  }
+  .image-2 {
+    height: 140px;
+  }
+  .image-3 {
+    height: 200px;
+  }
+  .image-4 {
+    height: 300px;
   }
 
-  .text-small {
+  .text,
+  .text-1,
+  .text-2,
+  .text-4,
+  .text-4 {
+    width: 100%;
+    height: .75rem;
+    border-radius: 0.25rem;
+  }
+
+  .title,
+  .title-1,
+  .title-2 {
+    width: 100%;
+    height: .75rem;
+    border-radius: 0.25rem;
+  }
+  .title-3 {
+    height: .8rem;
+  }
+  .title-4 {
+    height: 1rem;
+  }
+
+  .text-small,
+  .text-small-1 {
     width: 30%;
     height: 6px;
     border-radius: 2px;
+  }
+  .text-small-2 {
+    width: 40%;
+    height: 8px;
+    border-radius: 2px;
+  }
+  .text-small-3 {
+    width: 50%;
+    height: 10px;
+    border-radius: 3px;
+  }
+  .text-small-4 {
+    width: 70%;
+    height: 12px;
+    border-radius: 4px;
   }
 
   .paragraph {
@@ -69,32 +121,82 @@
     border-radius: 4px;
   }
 
-  .title {
+  .header,
+  .header-1 {
     width: 50%;
+    height: 18px;
+    margin-bottom: 12px;
+    border-radius: 0.25rem;
+  }
+  .header-2 {
+    width: 60%;
     height: 20px;
-    margin-bottom: 15px;
-    border-radius: 4px;
+    margin-bottom: 14px;
+    border-radius: 0.25rem;
+  }
+  .header-3 {
+    width: 70%;
+    height: 22px;
+    margin-bottom: 16px;
+    border-radius: 0.25rem;
+  }
+  .header-4 {
+    width: 80%;
+    height: 24px;
+    margin-bottom: 18px;
+    border-radius: 0.3rem;
   }
 
-  .avatar {
+  .avatar,
+  .avatar-1 {
     width: 40px;
     height: 40px;
     border-radius: 50%;
   }
+  .avatar-2 {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+  }
+  .avatar-3 {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+  .avatar-4 {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+  }
 
-  .thumbnail {
+  .thumbnail,
+  .thumbnail-1 {
     width: 40px;
     height: 40px;
     border-radius: 4px;
   }
-
-  .card {
-    display: flex;
-    flex-direction: column;
+  .thumbnail-2 {
+    width: 60px;
+    height: 60px;
+    border-radius: 4px;
+  }
+  .thumbnail-3 {
+    width: 80px;
+    height: 80px;
+    border-radius: 4px;
+  }
+  .thumbnail-4 {
+    width: 120px;
+    height: 120px;
+    border-radius: 4px;
+  }
+  .card,
+  .card-1,
+  .card-2,
+  .card-3,
+  .card-4 {
     border: 1px solid var(--color-gray-100);
     border-radius: 4px;
-    height: 300px;
-    width: 100%;
   }
 
   @media (min-width: 480px) {
@@ -105,10 +207,6 @@
 
   /* helpers */
 
-  .card-image {
-    background-color: var(--color-gray-100);
-    flex: 1 1 100px;
-  }
   .card-content {
     flex: 1 1 auto;
     padding: 1rem;
@@ -118,7 +216,21 @@
     display: flex;
     flex-direction: row;
     align-items: center;
+  }
+  .profile-1 {
     max-width: 280px;
+    gap: 1rem;
+  }
+  .profile-2 {
+    max-width: 360px;
+    gap: 1rem;
+  }
+  .profile-3 {
+    max-width: 480px;
+    gap: 1rem;
+  }
+  .profile-4 {
+    max-width: 560px;
     gap: 1rem;
   }
 
