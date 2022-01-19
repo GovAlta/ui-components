1282653bc96672986905c8ff16fdbfd47fcdc96f
Chris Olsen <olsen.chris@gmail.com>
2022-01-19 11:48:27 -0700

    add dummy test to existing spec file

---
+++ libs/web-components/src/components/app-header/AppHeader.spec.ts
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom';
+// import { render } from '@testing-library/svelte';
+
+// import GoAAppHeader from './AppHeader.svelte'
+
+describe('App Header', () => {
+  it('should render', async () => {
+    expect(true).toBe(true)
+  });
+});
