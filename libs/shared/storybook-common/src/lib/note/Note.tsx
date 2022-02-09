e871fb584dbd81d9756396a726c679cf025b0483
Chris Olsen <olsen.chris@gmail.com>
2022-02-09 11:10:13 -0700

    chore: override storybook's styling

--- libs/shared/storybook-common/src/lib/note/Note.tsx
+++ libs/shared/storybook-common/src/lib/note/Note.tsx
@@ -21,9 +21,9 @@ function getTitle(type: NoteType) {
 export const Note: FC<Props> = (props) => {
   return (
     <div className={ `note note-${props.type}` }>
-      <h4 className="note-title">
+      <h3 className="note-title">
         { getTitle(props.type) }
-      </h4>
+      </h3>
 
       <div className="note-content">
         {props.children}
