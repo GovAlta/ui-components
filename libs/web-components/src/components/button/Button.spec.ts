46687ae89cc22976dd5d91cb8f82b2899c6f28e3
Chris Olsen <olsen.chris@gmail.com>
2022-02-17 09:22:54 -0700

    chore: improve button tests and fix false pass

--- libs/web-components/src/components/button/Button.spec.ts
+++ libs/web-components/src/components/button/Button.spec.ts
@@ -3,84 +3,104 @@ import { render, fireEvent } from '@testing-library/svelte';
 import GoAButton from './Button.svelte'
 
 describe('GoAButtonComponent', () => {
-  const buttonText = 'Test Title';
-  const buttonPrimaryClassName = 'primary';
-  const buttonSecondaryClassName = 'secondary';
-  const buttonTertiaryClassName = 'tertiary';
-  const buttonSmallClassName = 'small';
-  const buttonTitle = 'hovering';
 
-  it('should render - success', async () => {
-    const baseElement = render(GoAButton, { disabled:"false", title: buttonText });
+  it('should render', async () => {
+    const buttonText = 'Test Title';
+    const baseElement = render(GoAButton, { title: buttonText });
     const button = await baseElement.findByRole('button');
 
     expect(button).toBeTruthy();
     expect(button).toContainHTML(buttonText)
   });
 
-  test('events should work', async () => {
-    const onClick = jest.fn();
-    const results = render(GoAButton);
+  describe("events", () => {
+    it('should handle the click event', async () => {
+      const onClick = jest.fn();
+      const results = render(GoAButton, { testid: 'button-test' });
+      const button = await results.findByTestId('button-test');
 
-    results.component.$on('click', onClick);
+      button.addEventListener('_click', onClick)
+      expect(button).not.toBeNull();
 
-    const button = results.container.querySelector('button');
-    expect(button).not.toBeNull();
+      await fireEvent.click(button);
+      expect(onClick).toBeCalled();
+    });
+  })
 
-    await fireEvent.click(button as HTMLElement);
+  describe("type", () => {
+    const buttonPrimaryClassName = 'primary';
+    const buttonSecondaryClassName = 'secondary';
+    const buttonTertiaryClassName = 'tertiary';
 
-    expect(results.container).toBeInTheDocument();
-    expect(onClick.mock.calls).not.toBeNull();
+    it('should render primary styling', async () => {
+      const baseElement = render(GoAButton, { type: "primary" });
+      const button = await baseElement.findByRole("button");
+
+      expect(button).toBeTruthy();
+
+      expect(button.className).toContain(buttonPrimaryClassName);
+      expect(button.className).not.toContain(buttonSecondaryClassName);
+      expect(button.className).not.toContain(buttonTertiaryClassName);
+    });
+
+    it('should render secondary styling', async () => {
+      const baseElement = render(GoAButton, { type: "secondary" });
+      const button = await baseElement.findByRole("button");
+
+      expect(button).toBeTruthy();
+
+      expect(button.className).not.toContain(buttonPrimaryClassName);
+      expect(button.className).toContain(buttonSecondaryClassName);
+      expect(button.className).not.toContain(buttonTertiaryClassName);
+    });
+
+    it('should render tertiary styling', async () => {
+      const baseElement = render(GoAButton, { type: "tertiary" });
+      const button = await baseElement.findByRole("button");
+
+      expect(button).toBeTruthy();
+
+      expect(button.className).not.toContain(buttonPrimaryClassName);
+      expect(button.className).not.toContain(buttonSecondaryClassName);
+      expect(button.className).toContain(buttonTertiaryClassName);
+    });
   });
 
-  it('type=should render primary styling', async () => {
-    const baseElement = render(GoAButton, { type:"primary", disabled:"false", title: buttonText });
-    const button = await baseElement.findByRole("button");
+  describe("size", () => {
+    it('should render small size', async () => {
+      const baseElement = render(GoAButton, { size: 'small' });
+      const button = await baseElement.findByRole("button");
 
-    expect(button).toBeTruthy();
+      expect(button).toBeTruthy();
+      expect(button.className).toContain("small");
+    });
 
-    expect(button.className).toContain(buttonPrimaryClassName);
-    expect(button.className).not.toContain(buttonSecondaryClassName);
-    expect(button.className).not.toContain(buttonTertiaryClassName);
+    it('should render medium size', async () => {
+      const baseElement = render(GoAButton, { size: 'medium' });
+      const button = await baseElement.findByRole("button");
+
+      expect(button).toBeTruthy();
+      expect(button.className).toContain("medium");
+    });
+
+    it('should render large size', async () => {
+      const baseElement = render(GoAButton, { size: 'large' });
+      const button = await baseElement.findByRole("button");
+
+      expect(button).toBeTruthy();
+      expect(button.className).toContain("large");
+    });
   });
 
-  it('type=should render secondary styling', async () => {
-    const baseElement = render(GoAButton, { type:"secondary", disabled:"false", title: buttonText });
-    const button = await baseElement.findByRole("button");
+  describe("title", () => {
+    it('should show button title', async () => {
+      const buttonTitle = 'hovering';
+      const baseElement = render(GoAButton, { title: buttonTitle });
+      const button = await baseElement.findByRole("button");
 
-    expect(button).toBeTruthy();
-
-    expect(button.className).not.toContain(buttonPrimaryClassName);
-    expect(button.className).toContain(buttonSecondaryClassName);
-    expect(button.className).not.toContain(buttonTertiaryClassName);
+      expect(button).toBeTruthy();
+      expect(button.title).toContain(buttonTitle);
+    });
   });
 
-  it('type=should render tertiary styling', async () => {
-    const baseElement = render(GoAButton, { type:"tertiary", disabled:"false", title: buttonText });
-    const button = await baseElement.findByRole("button");
-
-    expect(button).toBeTruthy();
-
-    expect(button.className).not.toContain(buttonPrimaryClassName);
-    expect(button.className).not.toContain(buttonSecondaryClassName);
-    expect(button.className).toContain(buttonTertiaryClassName);
-  });
-
-  test('size=small should render small styling', async () => {
-    const baseElement = render(GoAButton, { type:"tertiary", disabled:"false", title: buttonText, size:'small' });
-    const button = await baseElement.findByRole("button");
-
-    expect(button).toBeTruthy();
-
-    expect(button.className).toContain(buttonSmallClassName);
-  });
-
-  test('title is set to button title', async () => {
-    const baseElement = render(GoAButton, { type:"tertiary", disabled:"false", title:buttonTitle });
-    const button = await baseElement.findByRole("button");
-
-    expect(button).toBeTruthy();
-
-    expect(button.title).toContain(buttonTitle);
-  });
 });
