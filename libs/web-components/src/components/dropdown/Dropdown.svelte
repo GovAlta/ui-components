2d97de3b6e9e27dc8fbd6eb651c21d09d2e9802c
Chris Olsen <olsen.chris@gmail.com>
2022-04-22 11:25:10 -0600

    fix: close dropdown on blur

--- libs/web-components/src/components/dropdown/Dropdown.svelte
+++ libs/web-components/src/components/dropdown/Dropdown.svelte
@@ -94,6 +94,9 @@
     <div data-testid={`${name}-dropdown`}>
       <goa-input
         on:click={showMenu}
+        on:blur={async () => {
+          setTimeout(closeMenu, 100);
+        }}
         error={isError}
         disabled={disabled}
         {leadingicon}
