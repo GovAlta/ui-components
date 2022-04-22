29aa0b4d7efb0a3bb539bfcfe84963dae315598b
Chris Olsen <olsen.chris@gmail.com>
2022-04-22 10:12:32 -0600

    fix: remove multiselect and filtering for dropdown
    The dropdown was also greatly refactored, with the simplification of the communication between the dropdown
    and it's children. The downfall of this approach is that children can no longer be custom views.
    
    foo

--- libs/web-components/src/components/dropdown/Dropdown.spec.ts
+++ libs/web-components/src/components/dropdown/Dropdown.spec.ts
@@ -8,22 +8,24 @@ describe('GoADropdown', () => {
 
   it('should render', async () => {
     const items = ["red", "blue", "orange"];
-    const baseElement = render(GoADropdown, {
+    const result = render(GoADropdown, {
       testid: 'test-id',
       name: 'favcolor',
-      values: ['orange'],
+      value: 'orange',
       items,
     });
 
-    const dropDown = baseElement.queryByTestId("test-id");
-    const inputField = dropDown.querySelector('goa-input');
-    await fireEvent.focus(inputField);
+    const dropdown = result.queryByTestId("test-id");
+    const inputField = dropdown.querySelector('goa-input');
+    await fireEvent.click(inputField);
 
-    for (const item of items) {
-      const option = baseElement.queryByTestId(`dropdown-item-${item}`);
-      expect(option).toBeTruthy();
-      expect(option).toHaveTextContent(item);
-    }
+    await waitFor(() => {
+      for (const item of items) {
+        const option = result.queryByTestId(`${item}-dropdown-item`);
+        expect(option).toBeTruthy();
+        expect(option).toHaveTextContent(item);
+      }
+    });
   });
 
   describe("single selection", () => {
@@ -32,26 +34,29 @@ describe('GoADropdown', () => {
       const result = render(GoADropdown, {
         testid: 'test-id',
         name: 'favcolor',
-        values: ['orange'],
+        value: 'orange',
         items,
       });
 
-      const dropDown = result.queryByTestId("test-id");
-      const inputField = dropDown.querySelector('goa-input');
-      await fireEvent.focus(inputField);
+      const dropdown = result.queryByTestId("test-id");
+      const inputField = dropdown.querySelector('goa-input');
+      await fireEvent.click(inputField);
 
-      dropDown.addEventListener('_change', (e) => {
+      dropdown.addEventListener('_change', (e) => {
         const { name } = e["detail"];
         expect(name).toBe("favcolor");
       })
 
-      // validate comma delimited values
-      const input = result.container.querySelector("#favcolor-dropdown-input");
-      expect(input).toBeTruthy();
-      expect(input.getAttribute('value')).toContain('orange');
+      await waitFor(() => {
+        // validate input label
+        const input = result.container.querySelector("#favcolor-dropdown-input");
+        expect(input).toBeTruthy();
+        expect(input.getAttribute('value')).toContain('orange');
 
-      const option = result.queryByTestId("dropdown-item-orange");
-      expect(option).not.toBeNull();
+        // validate the selected item
+        const selected = result.container.querySelector(".goa-dropdown-option--selected");
+        expect(selected).not.toBeNull();
+      });
     });
 
     it('selects a value', async () => {
@@ -63,102 +68,26 @@ describe('GoADropdown', () => {
         items,
       });
 
-      const dropDown = result.queryByTestId("test-id");
-      const inputField = dropDown.querySelector('goa-input');
-      await fireEvent.focus(inputField);
+      const dropdown = result.queryByTestId("test-id");
+      const inputField = dropdown.querySelector('goa-input');
+      await fireEvent.click(inputField);
 
       let selectedValue = [];
 
-      dropDown.addEventListener('_change', (e) => {
+      dropdown.addEventListener('_change', (e) => {
         const { name, value } = e["detail"];
         selectedValue = value;
         expect(name).toBe("favcolor");
         onClick();
       })
 
-      // select item
-      const option = result.queryByTestId("dropdown-item-orange");
-      await fireEvent.click(option);
-      expect(onClick).toHaveBeenCalledTimes(1);
-      expect(selectedValue).toEqual(["orange"]);
-    });
-  })
-
-  describe("multi selection", () => {
-    it('has preselected values', async () => {
-      const items = ["red", "blue", "orange"];
-      const result = render(GoADropdown, {
-        testid: 'test-id',
-        name: 'favcolor',
-        values: '["orange", "red"]',
-        multiselect: true,
-        items,
+      await waitFor(async () => {
+        // select item
+        const option = result.queryByTestId("orange-dropdown-item");
+        await fireEvent.click(option);
+        expect(onClick).toHaveBeenCalledTimes(1);
+        expect(selectedValue).toEqual("orange");
       });
-
-      const dropDown = result.queryByTestId("test-id");
-      const inputField = dropDown.querySelector('goa-input');
-      await fireEvent.focus(inputField);
-
-      dropDown.addEventListener('_change', (e) => {
-        const { name } = e["detail"];
-        expect(name).toBe("favcolor");
-      })
-
-      // validate comma delimited values
-      const input = result.container.querySelector("#favcolor-dropdown-input");
-      expect(input).toBeTruthy();
-      expect(input.getAttribute('value')).toContain('red');
-      expect(input.getAttribute('value')).toContain('orange');
-
-      // validate they are selected
-      const orange = result.queryByTestId("dropdown-item-orange");
-      expect(orange).not.toBeNull();
-      const red = result.queryByTestId("dropdown-item-red");
-      expect(red).not.toBeNull();
-    });
-
-    it('selects values', async () => {
-      const onClick = jest.fn();
-      const items = ["red", "blue", "orange"];
-      const result = render(GoADropdown, {
-        testid: 'test-id',
-        name: 'favcolor',
-        values: [],
-        multiselect: "true",
-        items,
-      });
-
-      const dropDown = result.queryByTestId("test-id");
-      const inputField = dropDown.querySelector('goa-input');
-      await fireEvent.focus(inputField);
-
-      let selectedValue = [];
-
-      dropDown.addEventListener('_change', (e) => {
-        const { name, value } = e["detail"];
-        selectedValue = value;
-        expect(name).toBe("favcolor");
-        onClick();
-      })
-
-      // select first item
-      const option1 = result.queryByTestId("dropdown-item-orange");
-      await fireEvent.click(option1);
-      expect(onClick).toHaveBeenCalledTimes(1);
-      expect(selectedValue).toEqual(["orange"]);
-
-      // select second item
-      const option2 = result.queryByTestId("dropdown-item-red");
-      await fireEvent.click(option2);
-      expect(onClick).toHaveBeenCalledTimes(2);
-      expect(selectedValue).toContain("orange");
-      expect(selectedValue).toContain("red");
-
-      // deselect second item
-      await fireEvent.click(option2);
-      expect(onClick).toHaveBeenCalledTimes(3);
-      expect(selectedValue).toContain("orange");
-      expect(selectedValue).not.toContain("red");
     });
   })
 
@@ -171,8 +100,8 @@ describe('GoADropdown', () => {
       items,
     });
 
-    const dropDown = result.queryByTestId("test-id");
-    const inputField = dropDown.querySelector('goa-input');
+    const dropdown = result.queryByTestId("test-id");
+    const inputField = dropdown.querySelector('goa-input');
     await fireEvent.focus(inputField);
 
     const option = result.queryByTestId("dropdown-item-red");
@@ -188,11 +117,10 @@ describe('GoADropdown', () => {
       items,
     });
 
-    const dropDown = result.queryByTestId("test-id");
-    const inputField = dropDown.querySelector('goa-input');
+    const dropdown = result.queryByTestId("test-id");
+    const inputField = dropdown.querySelector('goa-input');
     await fireEvent.focus(inputField);
 
-    console.log(result.container.outerHTML);
     const option = result.container.querySelector("goa-input[error]");
     expect(option).not.toBeNull();
   });
@@ -206,39 +134,9 @@ describe('GoADropdown', () => {
       items,
     });
 
-    const dropDown = result.queryByTestId("test-id");
-    const input = dropDown.querySelector('goa-input');
+    const dropdown = result.queryByTestId("test-id");
+    const input = dropdown.querySelector('goa-input');
     expect(input).toHaveAttribute("leadingicon", "add");
   });
 
-  it.skip('allows for filtering of the items', async () => {
-    const items = ["red", "blue", "orange"];
-    const result = render(GoADropdown, {
-      testid: 'test-id',
-      name: 'favcolor',
-      filterable: "true",
-      items,
-    });
-
-    const dropDown = result.queryByTestId("test-id");
-    const inputField = dropDown.querySelector('goa-input');
-    await fireEvent.focus(inputField);
-    const filterField = dropDown.querySelector('#favcolor-dropdown-filter');
-
-    // FIXME: can't get a response to the changing of the goa-input filter value
-    filterField.setAttribute("value", "o");
-    dropDown.addEventListener('_change', (e) => {
-      const { name } = e["detail"];
-      expect(name).toBe("favcolor");
-    })
-
-    await waitFor(() => {
-      // select item
-      const orange = result.queryByTestId("dropdown-item-orange");
-      expect(orange).not.toBeNull();
-      const blue = result.queryByTestId("dropdown-item-blue");
-      expect(blue).toBeNull();
-    })
-
-  });
 });
