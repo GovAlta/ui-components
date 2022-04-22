d6a2a0fcff373a51653f7cac12bcd67588fd3769
Chris Olsen <olsen.chris@gmail.com>
2022-04-22 10:54:09 -0600

    refactor: simplify radio group parent/child communication

--- libs/web-components/src/components/radio-group/RadioGroup.spec.ts
+++ libs/web-components/src/components/radio-group/RadioGroup.spec.ts
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
 import { render, fireEvent, cleanup, waitFor } from '@testing-library/svelte';
-import { triggerAsyncId } from 'async_hooks';
 import GoARadioGroup from './RadioGroupWrapper.test.svelte';
 
 afterEach(cleanup);
@@ -9,41 +8,44 @@ describe('GoARadioGroup Component', () => {
 
   it('should render', async () => {
     const items = ["red", "blue", "orange"];
-    const baseElement = render(GoARadioGroup, {
+    const result = render(GoARadioGroup, {
       name: 'favcolor',
       value: 'orange',
       testid: 'test-id',
       items,
     });
 
-    const radioGroup = await baseElement.findByTestId('test-id');
-    for (const item of items) {
-      const radio = radioGroup.querySelector(`[data-testid=radio-item-${item}]`);
-      expect(radio).toBeTruthy();
-      const input = radio.querySelector('input');
-      expect(input).toHaveAttribute("name", "favcolor");
-    }
+    await waitFor(() => {
+      for (const item of items) {
+        const radio = result.queryByTestId(`radio-option-${item}`);
+        expect(radio).toBeTruthy();
+        const input = radio.querySelector('input');
+        expect(input).toHaveAttribute("name", "favcolor");
+      }
+    })
   });
 
   it('should handle the events', async () => {
     const items = ["red", "blue", "orange"];
-    const baseElement = render(GoARadioGroup, {
+    const result = render(GoARadioGroup, {
       name: 'favcolor',
       value: 'orange',
       testid: 'test-id',
       items,
     });
 
-    const radioGroup = await baseElement.findByTestId('test-id');
-    const orange = radioGroup.querySelector<HTMLInputElement>('input[type=radio][value=orange]');
-    const red = radioGroup.querySelector<HTMLInputElement>('input[type=radio][value=red]');
+    const radioGroup = await result.findByTestId('test-id');
 
     // initial state
-    expect(red.checked).toBe(false);
-    expect(orange.checked).toBe(true);
+    await waitFor(() => {
+      const orange = radioGroup.querySelector<HTMLInputElement>('input[type=radio][value=orange]');
+      const red = radioGroup.querySelector<HTMLInputElement>('input[type=radio][value=red]');
+      expect(red.checked).toBe(false);
+      expect(orange.checked).toBe(true);
 
-    fireEvent.click(red)
-    expect(red.checked).toBe(true);
-    expect(orange.checked).toBe(false);
+      fireEvent.click(red)
+      expect(red.checked).toBe(true);
+      expect(orange.checked).toBe(false);
+    });
   });
 });
