deba0e4390752c4ea26148c770329b2046abb45b
Rajan Babu Rajangam <rajanbabu.r@gmail.com>
2022-07-08 14:44:17 -0600

    chore(DDIDS-786): Build Container, form-item and skeleton components test cases for cypress test

---
+++ apps/angular-demo/src/app/container/container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'abgov-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.css']
+})
+export class ContainerComponent {
+
+  constructor() { }
+
+}
---
+++ apps/angular-demo/src/app/form-item/form-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'abgov-form-item',
+  templateUrl: './form-item.component.html',
+  styleUrls: ['./form-item.component.css']
+})
+export class FormItemComponent {
+
+  constructor() { }
+
+}
