3cece59919d747dadd022018393a46e0fa6ed6d4
Rajan Babu Rajangam <rajanbabu.r@gmail.com>
2022-08-11 23:33:35 -0600

    chore(DDI-736): Fix textarea character count code sample

--- apps/angular-demo/src/app/text-area/text-area.component.ts
+++ apps/angular-demo/src/app/text-area/text-area.component.ts
@@ -7,9 +7,22 @@ import { Component } from '@angular/core';
 })
 export class TextAreaComponent {
 
+  value2 = "";
+  value3 = "";
+
   constructor() { }
 
   onChange(e: any) {
+    console.log('changed', e.detail.name, e.detail.value);
+  }
+
+  onChange2(e: any) {
+    this.value2 = e.detail.value;
+    console.log('changed', e.detail.name, e.detail.value);
+  }
+
+  onChange3(e: any) {
+    this.value3 = e.detail.value;
     console.log('changed', e.detail.name, e.detail.value);
   }
 }
