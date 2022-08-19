31f954dcad42fb941977173dd2a02c866d440428
GovAlberta <107069489+GovAlberta@users.noreply.github.com>
2022-08-19 20:43:26 +0530

    chore(DDI-927):change half url to full url

--- apps/react-demo/src/routes/appHeader.tsx
+++ apps/react-demo/src/routes/appHeader.tsx
@@ -5,18 +5,18 @@ export default function AppHeader() {
   return (
     <>
       <h3>Basic</h3>
-      <GoAAppHeader url="example.com" />
+      <GoAAppHeader url="https://www.example.com" />
 
       <br />
       <h3>Title</h3>
       <GoAAppHeader
-        url="example.com"
+        url="https://www.example.com"
         heading="Ticket and Find Payments"
       ></GoAAppHeader>
 
       <br />
       <h3>Custom Content</h3>
-      <GoAAppHeader url="example.com" heading="Ticket and Find Payments">
+      <GoAAppHeader url="https://www.example.com" heading="Ticket and Find Payments">
         <a title="loginUrl" href="#">Login</a>
       </GoAAppHeader>
     </>
