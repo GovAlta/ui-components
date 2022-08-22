bd227741ab12f95af715b08d4dc39f02152c9dfe
GovAlberta <107069489+GovAlberta@users.noreply.github.com>
2022-08-23 00:32:24 +0530

    chore(DDI-962): Fix failing unit tests

--- libs/web-components/src/components/microsite-header/MicrositeHeader.spec.ts
+++ libs/web-components/src/components/microsite-header/MicrositeHeader.spec.ts
@@ -5,55 +5,55 @@ import GoAMicrositeHeader from './MicrositeHeader.svelte'
 describe('GoAMicrositeHeader', () => {
 
   it('should render with version', async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { level: "alpha", version: "1.2.3" });
-    const level = queryByTestId('level');
+    const { queryByTestId } = render(GoAMicrositeHeader, { type: "alpha", version: "1.2.3" });
+    const type = queryByTestId('type');
     const version = queryByTestId('version');
 
-    expect(level).toContainHTML("alpha");
+    expect(type).toContainHTML("alpha");
     expect(version).toContainHTML("1.2.3");
   });
 
   it('should render without a version', async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { level: "alpha" });
-    const level = queryByTestId('level');
+    const { queryByTestId } = render(GoAMicrositeHeader, { type: "alpha" });
+    const type = queryByTestId('type');
     const version = queryByTestId('version');
 
-    expect(level).toContainHTML("alpha");
+    expect(type).toContainHTML("alpha");
     expect(version).toBeNull();
   });
 
   it(`should render version number`, async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { level: 'alpha', version: "1.2.3" });
+    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'alpha', version: "1.2.3" });
     const version = queryByTestId('version');
 
     expect(version).toContainHTML("1.2.3");
   });
 
-  ['alpha', 'beta'].forEach(level => {
-    it(`should show the level for ${level}`, async () => {
-      const { queryByTestId } = render(GoAMicrositeHeader, { level: level });
-      const version = queryByTestId('level');
+  ['alpha', 'beta'].forEach(type => {
+    it(`should show the type for ${type}`, async () => {
+      const { queryByTestId } = render(GoAMicrositeHeader, { type: type });
+      const version = queryByTestId('type');
 
-      expect(version).toContainHTML(level);
+      expect(version).toContainHTML(type);
     });
   });
 
-  it(`should not show the level for live`, async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { level: 'live' });
-    const version = queryByTestId('level');
+  it(`should not show the type for live`, async () => {
+    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'live' });
+    const version = queryByTestId('type');
 
     expect(version).not.toContainHTML('live');
   });
 
   it(`should show a feedback url if provided`, async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { level: 'alpha', feedbackurl: 'http://example.com' });
+    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'alpha', feedbackurl: 'http://example.com' });
     const feedback = queryByTestId('feedback');
 
     expect(feedback).toContainHTML('http://example.com');
   });
 
   it(`should not show a feedback section if one isn't provided`, async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { level: 'alpha' });
+    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'alpha' });
     const feedback = queryByTestId('feedback');
 
     expect(feedback).toBeNull();
