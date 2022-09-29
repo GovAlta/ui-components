179bb1f1e2a0464e6244710f1f0e9e0f5bbd06cb
Dustin Nielsen <nielsen.dustin@gmail.com>
2022-09-28 18:46:03 -0600

    chore(DDI-1008): Renamed page component to one column layout
    Also moved the one column layout page to be under layouts.
    Modified the start page when loading the storybook to be the overview page.
    Fixed the issue with the one column layout page example falling into the tabs underneath.
    Changed the order of pages under Setup

--- libs/docs/.storybook/preview.js
+++ libs/docs/.storybook/preview.js
@@ -6,7 +6,7 @@ export const parameters = {
   },
   options: {
     storySort: {
-      order: [/^Overview/, 'Layouts', 'Components', 'Setup' ],
+      order: ['Overview', 'Layouts', 'Components', 'Setup', ['Contributors', 'Angular', 'React'] ],
     },
   },
 };
