98bc95252761c18cc6d1493816a3e7930acb48ff
Chris Olsen <olsen.chris@gmail.com>
2022-09-30 12:57:16 -0600

    chore(DDI-876): remove angular and vue libs

--- libs/angular-components/src/experimental/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './badge/badge.component';
-export * from './app-version-header/app-version-header.component';
-export * from './components.module';
-export * from './number-input/number-input.component';
--- libs/angular-components/src/lib/checkbox/checkbox-change.ts
+++
@@ -1,8 +0,0 @@
-import { GoACheckboxComponent } from './checkbox.component';
-
-export class GoACheckboxChange {
-    /** The source MatCheckbox of the event. */
-    source: GoACheckboxComponent;
-    /** The new `checked` value of the checkbox. */
-    checked: boolean;
-  }
