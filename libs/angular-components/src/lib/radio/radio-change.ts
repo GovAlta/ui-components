a3302a8d16e8c3292a591d92528e42b59cb155a4
Chris Olsen <olsen.chris@gmail.com>
2022-09-30 12:57:16 -0600

    chore(DDI-876): remove angular and vue libs

--- libs/angular-components/src/lib/radio/radio-change.ts
+++
@@ -1,8 +0,0 @@
-import { GoARadioComponent } from './radio.component';
-
-export class GoARadioChange {
-    /** The source radiobutton of the event. */
-    source: GoARadioComponent;
-    /** The new `checked` value of the radiobutton. */
-    checked: boolean;
-  }
--- libs/vue-components/README.md
+++
@@ -1,5 +0,0 @@
-# vue-components
-
-This library contains vue components from the Government of Alberta.
-
-Vue components for the latest version are currently being worked on.
