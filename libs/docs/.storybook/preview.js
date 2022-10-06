234e67d9e14216ccb09cc5748526e29a123200aa
Radha-Alberta <107069489+Radha-Alberta@users.noreply.github.com>
2022-10-06 16:34:29 -0600

    feat(DDI-1001): Adding form item examples (#910)
    * feat(DDI-1001): Adding form item examples
    
    * fix(DDI:1001) Changing padding pixel

--- libs/docs/.storybook/preview.js
+++ libs/docs/.storybook/preview.js
@@ -6,7 +6,7 @@ export const parameters = {
   },
   options: {
     storySort: {
-      order: ['Overview', 'Layouts', 'Components', 'Setup', ['Contributors', 'Angular', 'React'] ],
+      order: ['Overview', 'Layouts', 'Components', 'Utility', 'Setup', ['Contributors', 'Angular', 'React']],
     },
   },
 };
