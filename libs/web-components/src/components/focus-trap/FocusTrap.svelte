ada40730084ed8fbbcbca8a9343be3665f06d9cc
Chris Olsen <olsen.chris@gmail.com>
2022-10-11 13:23:11 -0600

    fix: prevent space being added to page at modal location when opened

--- libs/web-components/src/components/focus-trap/FocusTrap.svelte
+++ libs/web-components/src/components/focus-trap/FocusTrap.svelte
@@ -159,9 +159,15 @@
 
 </script>
 
-<div bind:this={element}>
+<div id="root" bind:this={element}>
   <slot />
   <!-- When goa-modal is the last display element in a page,
     the tab key press jumps out. The empty span with tabIndex="0" keeps the focus trapped mysteriously-->
   <span tabindex="0"></span>
 </div>
+
+<style>
+  #root {
+    display: inline;
+  }
+</style>
