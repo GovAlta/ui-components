51e26cb051e142f22a72507d6778fed2ec7a1162
Dustin Nielsen <nielsen.dustin@gmail.com>
2022-11-14 16:19:36 -0700

    chore: Updating the overview page to include additional links

--- apps/angular-demo/src/app/app-routing.module.ts
+++ apps/angular-demo/src/app/app-routing.module.ts
@@ -54,4 +54,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- libs/react-components/src/lib/notification/notification.spec.tsx
+++ libs/react-components/src/lib/notification/notification.spec.tsx
@@ -1,19 +1,23 @@
-import '@testing-library/jest-dom';
-import React from 'react';
-import { render } from '@testing-library/react';
-import GoANotification, {NotificationType} from './notification';
+import "@testing-library/jest-dom";
+import React from "react";
+import { render } from "@testing-library/react";
+import GoANotification, { NotificationType } from "./notification";
 
-describe('Notification Banner', () => {
-   
+describe("Notification Banner", () => {
   describe("type", () => {
-  ['important', 'information', 'emergency', 'event'].forEach((type: NotificationType) => {
-    it(`should render ${type} notification`, async function () {
-      render(<GoANotification type={type}>Information to the user goes in the content</GoANotification>);
-      const el = document.querySelector('goa-notification');
-      expect(el).not.toBeNull();
-      expect(el.getAttribute('type')).toEqual(type);
-    })
+    ["important", "information", "emergency", "event"].forEach(
+      (type: NotificationType) => {
+        it(`should render ${type} notification`, async function () {
+          render(
+            <GoANotification type={type}>
+              Information to the user goes in the content
+            </GoANotification>
+          );
+          const el = document.querySelector("goa-notification");
+          expect(el).not.toBeNull();
+          expect(el.getAttribute("type")).toEqual(type);
+        });
+      }
+    );
   });
-});
-
 });
