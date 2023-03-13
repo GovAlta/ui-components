3c3fa2093750dff12cd462b877574da7bd67fb39
amar-ABgov <amarnath.kothapalli@gov.ab.ca>
2023-03-13 17:25:33 -0400

    chore (DDI-1271): Update container docs

--- apps/react-demo/src/routes/container.tsx
+++ apps/react-demo/src/routes/container.tsx
@@ -1,9 +1,14 @@
+import { GoAContainer } from "@abgov/react-components";
 import * as React from "react";
 
 export default function Container() {
   return (
     <div>
       <h1>Container here</h1>
+      <GoAContainer>
+        <h1>Container here</h1>
+        <p>Some text</p>
+      </GoAContainer>
     </div>
   );
 }
