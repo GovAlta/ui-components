6fc24b64db799af8797f5fe367bfcd95890d40fe
Radha-Alberta <107069489+Radha-Alberta@users.noreply.github.com>
2023-12-28 07:53:20 -0700

    Adding data-cy attribute in angular apps

--- apps/angular-demo/src/app/tabs/tabs.ts
+++ apps/angular-demo/src/app/tabs/tabs.ts
@@ -1,7 +1,11 @@
 import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: "abgov-tabs",
   templateUrl: "./tabs.html",
 })
-export class TabsComponent {}
+export class TabsComponent {
+  colors: string[] = ['red', 'green', 'blue'];
+  reactiveFormCtrl = new FormControl("");
+}
