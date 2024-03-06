e78125bd9edd00915e19d479ff6641a0e39e442f
Chris Olsen <olsen.chris@gmail.com>
2024-03-06 11:46:30 -0700

    chore: devenv init

---
+++ devenv.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ 
+    pkgs.git 
+    pkgs.nodejs_21
+    pkgs.nodePackages_latest.svelte-language-server
+    pkgs.nodePackages_latest.typescript-language-server
+    pkgs.vscode-langservers-extracted
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo GoA UI Components";
+
+  enterShell = ''
+    git --version
+    node --version
+    echo
+    echo =================
+    echo GoA UI Components
+    echo =================
+  '';
+
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+  languages.typescript.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.shellcheck.enable = true;
+
+  dotenv.enable = true;
+}
