c98b23f0a4966a5a863985d7db00f6143fdf7c6f
Syed.Zeeshan <shariqzeeshan@gmail.com>
2024-06-13 17:34:23 -0400

    fix(#1840): link form-item to Input for accessibility

---
+++ libs/web-components/src/components/input/InputFormItemWrapper.test.svelte
@@ -0,0 +1,22 @@
+<svelte:options customElement="test-input-formitem-wrapper" />
+
+<!-- Script -->
+<script lang="ts">
+  import GoAFormItem from "../form-item/FormItem.svelte";
+  import GoAInput from "./Input.svelte";
+
+  export let label: string = "";
+  export let helptext: string = "";
+  export let testIdFormItem: string = "";
+
+  export let inputId: string = "";
+  export let testIdInput: string = "";
+  export let arialabel: string = "";
+  export let inputName: string = "";
+</script>
+
+<!-- HTML -->
+
+<GoAFormItem {label} {helptext} testid={testIdFormItem}>
+  <GoAInput id={inputId} name={inputName} {arialabel} testid={testIdInput} />
+</GoAFormItem>
