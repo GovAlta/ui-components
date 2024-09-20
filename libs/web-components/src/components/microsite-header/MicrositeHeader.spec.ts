27b0237e78636c8d8114f0f2426d90c4a408746d
Syed.Zeeshan <shariqzeeshan@gmail.com>
2024-09-20 16:26:48 -0400

    fix(#1897): microsite header add feedback click handler

--- libs/web-components/src/components/microsite-header/MicrositeHeader.spec.ts
+++ libs/web-components/src/components/microsite-header/MicrositeHeader.spec.ts
@@ -1,38 +1,43 @@
-import { render, cleanup } from '@testing-library/svelte';
-import GoAMicrositeHeader from './MicrositeHeader.svelte';
+import { render, cleanup, fireEvent } from "@testing-library/svelte";
+import GoAMicrositeHeader from "./MicrositeHeader.svelte";
 import GoAMicrositeHeaderWrapper from "./MicrositeHeaderWrapper.test.svelte";
 import { it, describe } from "vitest";
 
 afterEach(cleanup);
 
-describe('GoAMicrositeHeader', () => {
-
-  it('should render with version', async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { type: "alpha", version: "1.2.3" });
-    const type = queryByTestId('type');
-    const version = queryByTestId('version');
+describe("GoAMicrositeHeader", () => {
+  it("should render with version", async () => {
+    const { queryByTestId } = render(GoAMicrositeHeader, {
+      type: "alpha",
+      version: "1.2.3",
+    });
+    const type = queryByTestId("type");
+    const version = queryByTestId("version");
 
     expect(type).toContainHTML("alpha");
     expect(version).toContainHTML("1.2.3");
   });
 
-  it('should render without a version', async () => {
+  it("should render without a version", async () => {
     const { queryByTestId } = render(GoAMicrositeHeader, { type: "alpha" });
-    const type = queryByTestId('type');
-    const version = queryByTestId('version');
+    const type = queryByTestId("type");
+    const version = queryByTestId("version");
 
     expect(type).toContainHTML("alpha");
     expect(version).toBeNull();
   });
 
   it(`should render version number`, async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'alpha', version: "1.2.3" });
-    const version = queryByTestId('version');
+    const { queryByTestId } = render(GoAMicrositeHeader, {
+      type: "alpha",
+      version: "1.2.3",
+    });
+    const version = queryByTestId("version");
 
     expect(version).toContainHTML("1.2.3");
   });
 
-  it('should render slotted version', () => {
+  it("should render slotted version", () => {
     const el = render(GoAMicrositeHeaderWrapper, { hasSlottedVersion: true });
     const slot = el?.container.querySelector("[slot=version]");
     const slotContent = slot?.querySelector("span");
@@ -41,48 +46,95 @@ describe('GoAMicrositeHeader', () => {
     expect(slotContent?.innerHTML).toContain("foo");
   });
 
-  ['alpha', 'beta'].forEach(type => {
+  ["alpha", "beta"].forEach((type) => {
     it(`should show the type for ${type}`, async () => {
       const { queryByTestId } = render(GoAMicrositeHeader, { type: type });
-      const version = queryByTestId('type');
+      const version = queryByTestId("type");
 
       expect(version).toContainHTML(type);
     });
   });
 
   it(`should not show the type for live`, async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'live' });
-    const version = queryByTestId('type');
+    const { queryByTestId } = render(GoAMicrositeHeader, { type: "live" });
+    const version = queryByTestId("type");
 
-    expect(version).not.toContainHTML('live');
+    expect(version).not.toContainHTML("live");
   });
 
   it(`should show a feedback url if provided`, async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'alpha', feedbackurl: 'http://example.com' });
-    const feedback = queryByTestId('feedback');
-    expect(feedback).toContainHTML('http://example.com');
+    const { queryByTestId } = render(GoAMicrositeHeader, {
+      type: "alpha",
+      feedbackurl: "http://example.com",
+    });
+    const feedback = queryByTestId("feedback");
+    expect(feedback).toContainHTML("http://example.com");
   });
 
   it(`should not show a feedback section if one isn't provided`, async () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'alpha' });
-    const feedback = queryByTestId('feedback');
+    const { queryByTestId } = render(GoAMicrositeHeader, { type: "alpha" });
+    const feedback = queryByTestId("feedback");
 
     expect(feedback).toBeNull();
   });
 
-  it('should set target=_blank on all urls', () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'alpha', feedbackurl: 'http://example.com' });
-    const feedbackURL = queryByTestId('feedback').querySelector('a');
-    const headerURL = queryByTestId('site-text').querySelector('a');
-    expect(feedbackURL).toHaveAttribute('target', '_blank');
-    expect(headerURL).toHaveAttribute('target', '_blank');
+  it("should set target=_blank on all urls", () => {
+    const { queryByTestId } = render(GoAMicrositeHeader, {
+      type: "alpha",
+      feedbackurl: "http://example.com",
+    });
+    const feedbackURL = queryByTestId("feedback").querySelector("a");
+    const headerURL = queryByTestId("site-text").querySelector("a");
+    expect(feedbackURL).toHaveAttribute("target", "_blank");
+    expect(headerURL).toHaveAttribute("target", "_blank");
   });
 
-  it('should set target=_self when specified', () => {
-    const { queryByTestId } = render(GoAMicrositeHeader, { type: 'alpha', feedbackurl: 'http://example.com', feedbackurltarget: "self", headerurltarget: "self" });
-    const feedbackURL = queryByTestId('feedback').querySelector('a');
-    const headerURL = queryByTestId('site-text').querySelector('a');
-    expect(feedbackURL).toHaveAttribute('target', '_self');
-    expect(headerURL).toHaveAttribute('target', '_self');
+  it("should set target=_self when specified", () => {
+    const { queryByTestId } = render(GoAMicrositeHeader, {
+      type: "alpha",
+      feedbackurl: "http://example.com",
+      feedbackurltarget: "self",
+      headerurltarget: "self",
+    });
+    const feedbackURL = queryByTestId("feedback").querySelector("a");
+    const headerURL = queryByTestId("site-text").querySelector("a");
+    expect(feedbackURL).toHaveAttribute("target", "_self");
+    expect(headerURL).toHaveAttribute("target", "_self");
+  });
+});
+
+describe("GoAMicrositeHeader with click handler", () => {
+  it("should not show feedback link when feedbackurl and hasfeedbackhandler are not provided", () => {
+    const { queryByTestId } = render(GoAMicrositeHeader, { type: "alpha" });
+    const feedback = queryByTestId("feedback");
+    const feedbackClick = queryByTestId("feedback-click");
+
+    expect(feedback).toBeNull();
+    expect(feedbackClick).toBeNull();
+  });
+
+  it("should dispatch custom event when feedback link is clicked where hasfeedbackhandler is true and click handler is provided", async () => {
+    const mockClickHandler = vi.fn();
+    const { getByText } = render(GoAMicrositeHeader, {
+      type: "alpha",
+      hasfeedbackhandler: "true",
+    });
+
+    // Add event listener to capture the custom event
+    document.addEventListener("_feedbackClick", mockClickHandler);
+
+    const feedbackLink = getByText("feedback");
+    await fireEvent.click(feedbackLink);
+
+    expect(mockClickHandler).toHaveBeenCalledTimes(1);
+    expect(mockClickHandler.mock.calls[0][0]).toEqual(
+      expect.objectContaining({
+        type: "_feedbackClick",
+        bubbles: true,
+        composed: true,
+      }),
+    );
+
+    document.removeEventListener("_click", mockClickHandler);
   });
 });
--- libs/web-components/src/components/microsite-header/MicrositeHeader.svelte
+++ libs/web-components/src/components/microsite-header/MicrositeHeader.svelte
@@ -2,8 +2,9 @@
 
 <!-- Script -->
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onMount, tick } from "svelte";
   import { typeValidator } from "../../common/utils";
+  import { toBoolean } from "../../common/utils";
 
   // Validator
   const [Types, validateType] = typeValidator(
@@ -20,12 +21,17 @@
   export let maxcontentwidth = "100%";
   export let headerurltarget: UrlTargetType = "blank";
   export let feedbackurltarget: UrlTargetType = "blank";
+  export let hasfeedbackhandler: string = "false";
 
   // Validator
   const [UrlTarget, validateUrlTargetType] = typeValidator(
     "URL target values",
     ["self", "blank"],
   );
+
+  $: _hasfeedbackhandler = toBoolean(hasfeedbackhandler);
+
+  let _feedbackElement: HTMLElement;
 
   // Types
   type UrlTargetType = (typeof UrlTarget)[number];
@@ -35,7 +41,17 @@
     return val[0].toUpperCase() + val.slice(1);
   }
 
-  onMount(() => {
+  function handleFeedbackClick(event: MouseEvent) {
+    if (_hasfeedbackhandler == true) {
+      event.preventDefault();
+
+      _feedbackElement.dispatchEvent(
+        new CustomEvent("_feedbackClick", { composed: true, bubbles: true }),
+      );
+    }
+  }
+
+  onMount(async () => {
     setTimeout(() => validateType(type), 1);
     validateUrlTargetType(headerurltarget);
     validateUrlTargetType(feedbackurltarget);
@@ -70,13 +86,18 @@
           target={`_${headerurltarget}`}>Alberta Government</a
         >
         service
+
         {#if feedbackurl}
-          <span data-testid="feedback"
-            >— help us improve it by giving <a
-              href={feedbackurl}
-              target={`_${feedbackurltarget}`}>feedback</a
-            ></span
-          >
+          <span data-testid="feedback">
+            — help us improve it by giving
+            <a href={feedbackurl} target={`_${feedbackurltarget}`}>feedback</a>
+          </span>
+        {:else if _hasfeedbackhandler}
+          <span data-testid="feedback-click" bind:this={_feedbackElement}>
+            — help us improve it by giving
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a href="#" on:click={handleFeedbackClick}>feedback</a>
+          </span>
         {/if}
       </div>
     {/if}
@@ -137,7 +158,6 @@
   }
 
   .content-container {
-
     font-size: var(--goa-font-size-2);
     padding: var(--goa-space-xs) var(--goa-space-m);
 
