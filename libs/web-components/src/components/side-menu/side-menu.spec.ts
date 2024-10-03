77792379604c1ebbdc1815c44a35d56cf68afbe3
Syed.Zeeshan <shariqzeeshan@gmail.com>
2024-10-03 17:51:18 -0400

    fix(#1973): side menu active link issues

--- libs/web-components/src/components/side-menu/side-menu.spec.ts
+++ libs/web-components/src/components/side-menu/side-menu.spec.ts
@@ -1,6 +1,7 @@
-import { it } from "vitest";
+import { it, expect } from "vitest";
 import { render } from "@testing-library/svelte";
 import SideMenuWrapper from "./SideMenuWrapper.test.svelte";
+import { tick } from "svelte";
 
 describe.skip("SideMenu should render with children and set highlighted menu item correctly", () => {
   it("should render", async () => {
@@ -18,5 +19,28 @@ describe.skip("SideMenu should render with children and set highlighted menu ite
     const currentLink = container.querySelector("a.current");
     expect(currentLink).toBeTruthy();
     expect(currentLink?.getAttribute("href")).toBe("get-started");
+  });
+});
+
+describe("SideMenu", () => {
+  let originalLocation: Location;
+
+  it("should set active link correctly", async () => {
+    originalLocation = window.location;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/no-explicit-any
+    window.location = new URL("http://localhost/get-started/designers") as any;
+
+    const { container } = render(SideMenuWrapper);
+    await tick();
+
+    const links = container.querySelectorAll("a");
+    expect(links.length).toBe(4);
+    const currentLink = container.querySelector("a.current");
+
+    expect(currentLink).toBeTruthy();
+    expect(currentLink?.getAttribute("href")).not.toBe("get-started");
+    expect(currentLink?.getAttribute("href")).toBe("get-started/designers");
+
+    window.location = originalLocation;
   });
 });
