86d7512da3070f84e02936a6c60603c13fc63b65
Vanessa Tran <Vanessa.M.Tran@gov.ab.ca>
2024-10-22 16:17:06 -0600

    feat(#2177): add click event for app header toggle menu

--- libs/react-components/src/lib/app-header/app-header.spec.tsx
+++ libs/react-components/src/lib/app-header/app-header.spec.tsx
@@ -10,4 +10,15 @@ describe("GoAAppHeader", () => {
     const header = baseElement.querySelector("goa-app-header");
     expect(header).toBeTruthy();
   });
+  it("should dispatch onMobileMenuClick if provided", () => {
+    const onMobileMenuClick = vi.fn();
+    const { baseElement } = render(
+      <GoAAppHeader heading="Test heading" url="test" onMenuClick={onMobileMenuClick} />
+    );
+
+    const header = baseElement.querySelector("goa-app-header");
+    expect(header).toBeTruthy();
+    header?.dispatchEvent(new Event("_menuClick"));
+    expect(onMobileMenuClick).toHaveBeenCalled();
+  })
 });
--- libs/react-components/src/lib/side-menu-group/side-menu-group.spec.tsx
+++ libs/react-components/src/lib/side-menu-group/side-menu-group.spec.tsx
@@ -10,4 +10,12 @@ describe("SideMenuGroup", () => {
     expect(el?.getAttribute("heading")).toBe("some header");
     expect(el?.getAttribute("testid")).toBe("foo");
   });
+  it("should render icon if provided", () => {
+    const { baseElement } = render(
+      <SideMenuGroup heading={"Some header"} testId={"foo"} icon={"accessibility"} />,
+    );
+
+    const el = baseElement.querySelector("goa-side-menu-group");
+    expect(el?.getAttribute("icon")).toBe("accessibility");
+  })
 });
