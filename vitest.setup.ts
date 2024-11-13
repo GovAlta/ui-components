929d6f754c7f8b70665ab68ac1eede5856c8568d
Chris Olsen <olsen.chris@gmail.com>
2024-11-13 16:21:00 -0700

    fix: ignore certain error text in test output

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -5,3 +5,12 @@ import { expect } from "vitest";
 
 expect.extend(matchers);
 expect.extend(moreExpect);
+
+// prevent "Could not parse CSS stylesheet" errors from showing up when running tests
+const originalConsoleError = console.error;
+const jsDomCssError = "Error: Could not parse CSS stylesheet";
+console.error = (...params) => {
+  if (!params.find((p) => p.toString().includes(jsDomCssError))) {
+    originalConsoleError(...params);
+  }
+};
