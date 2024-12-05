dacea0d88b2e0b549d2dce1c795fdfca82adfb9c
b2 <brendan@mobilewebsmart.com>
2024-12-05 11:47:30 -0700

    cleanup

--- libs/react-components/src/lib/textarea/textarea.spec.tsx
+++ libs/react-components/src/lib/textarea/textarea.spec.tsx
@@ -19,8 +19,10 @@ describe("TextArea", () => {
         mr="m"
         mb="l"
         ml="xl"
-        onChange={() => { /* do nothing */ }}
-      />
+        onChange={() => {
+          /* do nothing */
+        }}
+      />,
     );
 
     const el = document.querySelector("goa-textarea");
@@ -56,7 +58,7 @@ describe("TextArea", () => {
           expect(value).toBe(newValue);
           onChange();
         }}
-      />
+      />,
     );
 
     const el = document.querySelector("goa-textarea");
@@ -65,7 +67,7 @@ describe("TextArea", () => {
       el,
       new CustomEvent("_change", {
         detail: { name: "textarea-name", value: newValue },
-      })
+      }),
     );
 
     expect(onChange).toBeCalled();
