bcf61fdd64caaf4188d4b4db5813218e240133c0
Thomas Jeffery <thomaswjeffery@gmail.com>
2024-12-06 17:01:33 -0700

    Fix: styling on table column sort

--- libs/web-components/src/components/table/TableSortHeader.svelte
+++ libs/web-components/src/components/table/TableSortHeader.svelte
@@ -55,8 +55,8 @@
     font-weight: inherit;
     color: inherit;
     width: 100%;
-    height: 3.75rem;
-    padding: 0 1rem;
+    height: 3rem;
+    padding: 12px 1rem;
     justify-content: var(--header-text-align, flex-start);
     gap: 0.25rem;
     align-items: flex-end;
@@ -80,7 +80,7 @@
     color: var(--goa-color-greyscale-400);
   }
   button:hover .direction--none goa-icon {
-    color: var(--goa-color-greyscale-400);
+    color: var(--goa-color-interactive-hover);
   }
 
   goa-icon {
