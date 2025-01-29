6ab007e372d1d86811f42504a550ab7c9f63687e
syedszeeshan <47701214+syedszeeshan@users.noreply.github.com>
2025-01-29 18:56:39 -0500

    fix(#2345): appheader links with query strings not showing as active

--- libs/web-components/src/common/urls.ts
+++ libs/web-components/src/common/urls.ts
@@ -2,22 +2,51 @@ export function isUrlMatch(windowUrl: URL | Location, testUrl: string): number {
   // path match
   if (testUrl === undefined) return -1;
 
-  const windowUrlParts = windowUrl.pathname.replace(/^\//, "").split("/");
-  const urlParts = testUrl.replace(/^\//, "").split("/");
-
-  if (windowUrlParts.length < urlParts.length) {
-    return -1;
-  }
-
-  // hash
+  // Handle hash links
   if (windowUrl.hash.length > 0 && windowUrl.hash === testUrl) {
     return 1;
+  }
+
+  // Parse the test URL
+  let parsedTestUrl: URL;
+  try {
+    parsedTestUrl = new URL(testUrl, windowUrl.origin);
+  } catch {
+    // If parsing fails, fall back to path-only comparison
+    const windowUrlParts = windowUrl.pathname.replace(/^\//, "").split("/");
+    const urlParts = testUrl.replace(/^\//, "").split("/");
+
+    if (windowUrlParts.length < urlParts.length) {
+      return -1;
+    }
+
+    let weight = -1;
+    let index = 0;
+
+    for (const part of urlParts) {
+      if (windowUrlParts[index] !== part) {
+        return -1;
+      }
+      weight += 1;
+      index++;
+    }
+
+    return weight >= 0 ? weight + 1 : weight;
+  }
+
+  // Compare pathnames
+  const windowUrlParts = windowUrl.pathname.replace(/^\//, "").split("/");
+  const testUrlParts = parsedTestUrl.pathname.replace(/^\//, "").split("/");
+
+  if (windowUrlParts.length < testUrlParts.length) {
+    return -1;
   }
 
   let weight = -1;
   let index = 0;
 
-  for (const part of urlParts) {
+  // Compare path parts
+  for (const part of testUrlParts) {
     if (windowUrlParts[index] !== part) {
       // Ex: windowURl: /get-started/designers should match to a menu "/#/get-started, but not match to "/get-started/developers
       // So if we check by menu (linkParts) and have anything not matched, it should return -1 (not matched), otherwise menu /get-started/developers & menu /get-started/ will have the same weight
@@ -28,6 +57,29 @@ export function isUrlMatch(windowUrl: URL | Location, testUrl: string): number {
   }
 
   // if weight was incremented once, then it actually has a value of 1, not 0
+  // Compare query parameters if present in test URL
+  const testParams = new URLSearchParams(parsedTestUrl.search);
+  const windowParams = new URLSearchParams(windowUrl.search);
+
+  if (testParams.toString()) {
+    let allParamsMatch = true;
+    for (const [key, value] of testParams) {
+      if (windowParams.get(key) !== value) {
+        allParamsMatch = false;
+        break;
+      }
+    }
+
+    // If query parameters are specified but don't match, return -1
+    if (!allParamsMatch) {
+      return -1;
+    }
+
+    // Add additional weight for matching query parameters
+    weight += testParams.toString().length > 0 ? 1 : 0;
+    //weight += 1;
+  }
+
   return weight >= 0 ? weight + 1 : weight;
 }
 
