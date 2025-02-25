ae4933e4adf19c074f688c5005de950bc9d5b9ce
Chris Olsen <olsen.chris@gmail.com>
2025-02-25 10:40:22 -0700

    chore: public-form refactor

--- libs/web-components/src/components/form/SubForm.svelte
+++ libs/web-components/src/components/form/SubForm.svelte
@@ -206,7 +206,7 @@
     _state = data;
 
     // dispatch up to the form-utils to ensure it's state is in sync
-    dispatch(_relayEl, "_stateChange", { data: _state, id }, { bubbles: true });
+    dispatchState();
   }
 
   // Collect list of child form item (input, dropdown, etc) elements
@@ -275,14 +275,18 @@
 
     // stop original event to prevent just the single subform data from being sent, and
     // send the array of data instead
+    dispatchState();
+
+    // initial event will be overridden with a custom _stateChange event containing a state array
+    e.stopPropagation();
+  }
+
+  function dispatchState() {
     dispatch<StateChangeRelayDetail>(_relayEl, StateChangeEvent, {
       type: "list",
       data: _state,
       id
     }, { bubbles: true });
-
-    // initial event will be overridden with a custom _stateChange event containing a state array
-    e.stopPropagation();
   }
 
   /**
