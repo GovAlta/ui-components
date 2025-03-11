5865e341e36bac07337e84b9a1784fb9827ca560
Benji Franck <benjamin.franck@gmail.com>
2025-03-11 13:17:22 -0600

    feat(#2478): add work side menu component

---
+++ libs/web-components/src/components/work-side-menu/WorkSideMenu.spec.ts
@@ -0,0 +1,104 @@
+import { it, expect } from "vitest";
+import { fireEvent, getByTestId, render, waitFor } from "@testing-library/svelte";
+import WorkSideMenuWrapper from "./WorkSideMenuWrapper.test.svelte";
+import { tick } from "svelte";
+
+
+describe("WorkSideMenu", () => {
+
+  const menuOptions = {
+    title: "Test menu",
+    url: "https://localhost/",
+    username: "Test User",
+    useremail: "test@example.com",
+    testid: "work-side-menu"
+  };
+
+  it("renders", async () => {
+    const { container } = render(WorkSideMenuWrapper, menuOptions);
+
+    const links = container.querySelectorAll("a.menu-item");
+    expect(links.length).toBe(4);
+  });
+
+  it("toggles the menu when clicking the toggle button", async () => {
+    const el = render(WorkSideMenuWrapper, menuOptions);
+    expect(el).toBeTruthy();
+
+    const menu = await el.findByTestId("work-side-menu");
+    expect(menu).toBeTruthy();
+
+    const toggleButton = await el.findByTestId("work-side-menu-toggle-button")
+    expect(toggleButton).toBeTruthy();
+
+    // initial state
+    expect(menu.classList.contains("closed")).toBeFalsy();
+
+    // close
+    await fireEvent.click(toggleButton);
+    expect(menu.classList.contains("closed")).toBeTruthy();
+
+    // open
+    await fireEvent.click(toggleButton);
+    expect(menu.classList.contains("closed")).toBeFalsy();
+  });
+
+  describe("Mobile", () => {
+
+    beforeEach(() => {
+      Object.defineProperty(window, "innerWidth", {
+        writable: true,
+        value: 480,
+      });
+    });
+
+    it("renders", async () => {
+      const el = render(WorkSideMenuWrapper, menuOptions);
+      expect(el).toBeTruthy();
+
+      const menu = await el.findByTestId("work-side-menu");
+      expect(menu).toBeTruthy();
+      expect(menu.classList.contains("mobile")).toBeTruthy();
+
+    });
+
+    it("closes when clicking the background", async () => {
+      const el = render(WorkSideMenuWrapper, menuOptions);
+      expect(el).toBeTruthy();
+
+      const menu = await el.findByTestId("work-side-menu");
+      expect(menu).toBeTruthy();
+
+      const background = await el.findByTestId("work-side-menu-background");
+      expect(background).toBeTruthy();
+
+      // initial state
+      expect(menu.classList.contains("closed")).toBeFalsy();
+
+      // close
+      await fireEvent.click(background);
+      expect(menu.classList.contains("closed")).toBeTruthy();
+    });
+
+    it("toggles menu when firing a toggle event", async () => {
+      const el = render(WorkSideMenuWrapper, menuOptions);
+      expect(el).toBeTruthy();
+
+      const menu = await el.findByTestId("work-side-menu");
+      expect(menu).toBeTruthy();
+
+      // initial state
+      expect(menu.classList.contains("closed")).toBeFalsy()
+
+      // close
+      menu.dispatchEvent(new CustomEvent("work-side-menu:toggle"));
+      await waitFor(() => expect(menu.classList.contains("closed")).toBeTruthy(), {timeout: 1} );
+
+      // open
+      menu.dispatchEvent(new CustomEvent("work-side-menu:toggle"));
+      await waitFor(() => expect(menu.classList.contains("closed")).toBeFalsy(), {timeout: 1} );
+    });
+  });
+
+
+});
