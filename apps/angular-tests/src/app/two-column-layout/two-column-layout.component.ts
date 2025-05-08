2d3b84410714e0bc713e2fb2cf5bddaf659713ba
Chris Olsen <chrisolsen@users.noreply.github.com>
2025-05-08 15:34:08 -0600

    chore: remove no longer used apps and dependencies

--- apps/angular-tests/src/app/two-column-layout/two-column-layout.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "goab-two-column-layout",
-  templateUrl: "./two-column-layout.component.html",
-})
-export class TwoColumnLayoutComponent {
-  constructor() {}
-}
