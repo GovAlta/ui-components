e3a5cf650c5a74f032e9e4532635e0a5a2e2b79a
Chris Olsen <chrisolsen@users.noreply.github.com>
2025-05-08 12:38:53 -0600

    chore: minimize the amount of template code
    Mainly to make it easier to use ide's fuzzy finder as with all these files the files we wanted to find were often in the middle of the matched files.

--- _templates/angular/src/app/app.component.ts
+++ _templates/angular/src/app/app.component.ts
@@ -1,49 +1,11 @@
 import {Component, OnInit} from "@angular/core";
-import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
-import {Countries, CountrySubdivisions} from "playground/angular/src/app/countries.data";
 
 @Component({
   selector: "goab-root",
-  templateUrl: "./app.component.html",
-  styles: ``,
+  template: "<abgov-component-wrapper></abgov-component-wrapper>"
 })
 export class AppComponent implements OnInit {
-  countries = Countries;
-  subdivisions = CountrySubdivisions;
-
-  fg: FormGroup<{
-    businessName: FormControl<string | null>;
-    address1: FormControl<string | null>;
-    address2: FormControl<string | null>;
-    locality: FormControl<string | null>;
-    region: FormControl<string | null>;
-    country: FormControl<string | null>;
-    postalCode: FormControl<string | null>;
-  }>;
-
-  constructor(
-    private fb: FormBuilder,
-  ) {
-    this.fg = this.fb.group({
-      businessName: [null as (string|null), []],
-      address1: [null as (string|null), []],
-      address2: [null as (string|null), []],
-      locality: [null as (string|null), []],
-      region: [null as (string|null), []],
-      country: [null as (string|null), []],
-      postalCode: [null as (string|null), []]
-    });
-  }
   ngOnInit() {
-    this.fg.get('country')?.valueChanges.subscribe((value) => {
-      console.log("Hey country is changed ", value);
-    });
-    this.fg.get('region')?.valueChanges.subscribe((value) => {
-      console.log("region is changed  ", value);
-    });
-  }
-
-  submit() {
-    console.log("Form  value is ", this.fg.value);
+    console.log("Hello from Angular");
   }
 }
