f7f324e1950fca0735be3d415e2b0db56ab3170d
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-05-21 16:14:52 -0600

    feat(#2465): react public forms

--- libs/react-components/src/lib/form-item/form-item.spec.tsx
+++ libs/react-components/src/lib/form-item/form-item.spec.tsx
@@ -14,6 +14,8 @@ describe("GoaFormItem", () => {
         helpText="This is some help text"
         maxWidth="480px"
         id="firstName"
+        name="first_name"
+        publicFormSummaryOrder={1}
       />
     );
     const el = baseElement.querySelector("goa-form-item");
@@ -24,5 +26,33 @@ describe("GoaFormItem", () => {
     expect(el?.getAttribute("helptext")).toEqual("This is some help text");
     expect(el?.getAttribute("maxwidth")).toEqual("480px");
     expect(el?.getAttribute("id")).toEqual("firstName");
+    expect(el?.getAttribute("name")).toEqual("first_name");
+    expect(el?.getAttribute("public-form-summary-order")).toEqual("1");
+  });
+
+  it("renders without optional properties", () => {
+    const { baseElement } = render(
+      <GoabFormItem
+        label="First Name"
+      />
+    );
+    const el = baseElement.querySelector("goa-form-item");
+    expect(el?.getAttribute("label")).toEqual("First Name");
+    expect(el?.getAttribute("name")).toBeNull();
+    expect(el?.getAttribute("public-form-summary-order")).toBeNull();
+  });
+
+  it("renders with only public form properties", () => {
+    const { baseElement } = render(
+      <GoabFormItem
+        label="First Name"
+        name="first_name"
+        publicFormSummaryOrder={2}
+      />
+    );
+    const el = baseElement.querySelector("goa-form-item");
+    expect(el?.getAttribute("label")).toEqual("First Name");
+    expect(el?.getAttribute("name")).toEqual("first_name");
+    expect(el?.getAttribute("public-form-summary-order")).toEqual("2");
   });
 });
