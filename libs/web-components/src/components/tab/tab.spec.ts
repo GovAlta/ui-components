8f42679801f3a5d025de761cb07f515eaf87986a
b2 <brendan@mobilewebsmart.com>
2025-07-07 13:15:54 -0600

    fix(#2331): ensure slot exists

--- libs/web-components/src/components/tab/tab.spec.ts
+++ libs/web-components/src/components/tab/tab.spec.ts
@@ -5,11 +5,11 @@ import { it, expect } from "vitest";
 it('should render tabpanel when open is set', async () => {
   const result = render(GoATab, { heading: 'Title', open: true });
   expect(result.container.innerHTML).toContain("Title");
-  expect(result.container.querySelector('div[role="tabpanel"]')).toBeTruthy();
+  expect(result.container.querySelector('div[role="tabpanel"]')).toBeVisible();
 });
 
 it('should not render tabpanel when open is not true', async () => {
   const result = render(GoATab, { heading: 'Title' });
   expect(result.container.innerHTML).toContain("Title");
-  expect(result.container.querySelector('div[role="tabpanel"]')).not.toBeTruthy();
+  expect(result.container.querySelector('div[role="tabpanel"]')).not.toBeVisible();
 });
