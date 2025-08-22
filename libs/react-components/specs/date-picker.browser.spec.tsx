b69ab6d70491d32a329de3b926697f751957de18
Dustin Nielsen <nielsen.dustin@gmail.com>
2025-08-22 14:30:54 -0600

    fix(#2682): Modified react wrapper to be the same as angular wrapper

--- libs/react-components/specs/date-picker.browser.spec.tsx
+++ libs/react-components/specs/date-picker.browser.spec.tsx
@@ -1,4 +1,5 @@
 import { render } from "vitest-browser-react";
+import React from "react";
 
 import { GoabDatePicker } from "../src";
 import { expect, describe, it, vi } from "vitest";
@@ -9,74 +10,104 @@ describe("Date Picker input type", () => {
     const Component = () => {
       return (
         <div data-testid={"container"}>
-          <GoabDatePicker type="input" name="datePickerInputType" testId="datePicker"></GoabDatePicker>
+          <GoabDatePicker
+            type="input"
+            name="datePickerInputType"
+            testId="datePicker"
+          ></GoabDatePicker>
         </div>
       );
     };
 
     const result = render(<Component />);
-		const datePickerMonth = result.getByTestId("month-dropdown");
-		const datePickerDay = result.getByTestId("day-input");
-		const datePickerYear = result.getByTestId("year-input");
+    const datePickerMonth = result.getByTestId("month-dropdown");
+    const datePickerDay = result.getByTestId("day-input");
+    const datePickerYear = result.getByTestId("year-input");
 
-		expect(datePickerMonth).toBeTruthy();
-		expect(datePickerDay).toBeTruthy();
-		expect(datePickerYear).toBeTruthy();
+    expect(datePickerMonth).toBeTruthy();
+    expect(datePickerDay).toBeTruthy();
+    expect(datePickerYear).toBeTruthy();
 
-		const rootElChangeHandler = vi.fn();
-		result.container.addEventListener("_change", (e: Event) => {
-			const ce = e as CustomEvent;
-			rootElChangeHandler(ce.detail.value);
-		});
+    const rootElChangeHandler = vi.fn();
+    result.container.addEventListener("_change", (e: Event) => {
+      const ce = e as CustomEvent;
+      rootElChangeHandler(ce.detail.value);
+    });
 
-		// Select month
-		if (datePickerMonth) {
-			await datePickerMonth.click();
-			await userEvent.keyboard("{ArrowDown}");
-			await userEvent.keyboard("{Enter}");
-		}
+    // Select month
+    if (datePickerMonth) {
+      await datePickerMonth.click();
+      await userEvent.keyboard("{ArrowDown}");
+      await userEvent.keyboard("{Enter}");
+    }
 
-		// should be null because date is invalid
-		await vi.waitFor(() => {
-			expect(rootElChangeHandler).toHaveBeenCalledWith(null);
-		});
-		rootElChangeHandler.mockClear();
+    // should be null because date is invalid
+    await vi.waitFor(() => {
+      expect(rootElChangeHandler).toHaveBeenCalledWith(null);
+    });
+    rootElChangeHandler.mockClear();
 
-		// Input day
-		if (datePickerDay) {
-			await datePickerDay.click();
-			await userEvent.type(datePickerDay, "1");
-		}
+    // Input day
+    if (datePickerDay) {
+      await datePickerDay.click();
+      await userEvent.type(datePickerDay, "1");
+    }
 
-		// should be null because date is still invalid
-		await vi.waitFor(() => {
-			expect(rootElChangeHandler).toHaveBeenCalledWith(null);
-		});
-		rootElChangeHandler.mockClear();
+    // should be null because date is still invalid
+    await vi.waitFor(() => {
+      expect(rootElChangeHandler).toHaveBeenCalledWith(null);
+    });
+    rootElChangeHandler.mockClear();
 
-		// Input year
-		if (datePickerYear) {
-			await datePickerYear.click();
-			await userEvent.type(datePickerYear, "1999");
-		}
+    // Input year
+    if (datePickerYear) {
+      await datePickerYear.click();
+      await userEvent.type(datePickerYear, "1999");
+    }
 
-		// should not be null because date became valid
-		await vi.waitFor(() => {
-			expect(rootElChangeHandler).toHaveBeenCalledWith("1999-01-01");
-		});
-		rootElChangeHandler.mockClear();
+    // should not be null because date became valid
+    await vi.waitFor(() => {
+      expect(rootElChangeHandler).toHaveBeenCalledWith("1999-01-01");
+    });
+    rootElChangeHandler.mockClear();
 
-		// Clear day input
-		if (datePickerDay) {
-			await datePickerDay.click();
-			await userEvent.keyboard("{ArrowRight}");
-			await userEvent.keyboard("{Backspace}");
-		}
+    // Clear day input
+    if (datePickerDay) {
+      await datePickerDay.click();
+      await userEvent.keyboard("{ArrowRight}");
+      await userEvent.keyboard("{Backspace}");
+    }
 
-		// should be null because date became invalid
-		await vi.waitFor(() => {
-			expect(rootElChangeHandler).toHaveBeenCalledWith(null);
-		});
-		rootElChangeHandler.mockClear();
-	});
+    // should be null because date became invalid
+    await vi.waitFor(() => {
+      expect(rootElChangeHandler).toHaveBeenCalledWith(null);
+    });
+    rootElChangeHandler.mockClear();
+  });
+
+  it("should have disabled property on input when disabled is true and type is input", async () => {
+    const Component = () => {
+      return (
+        <div data-testid={"container"}>
+          <GoabDatePicker
+            type="input"
+            name="datePickerInputType"
+            testId="datePicker"
+            disabled={true}
+          ></GoabDatePicker>
+        </div>
+      );
+    };
+
+    const result = render(<Component />);
+    const monthInput = result.getByTestId("input");
+    const datePickerDay = result.getByTestId("day-input");
+    const datePickerYear = result.getByTestId("year-input");
+
+    await vi.waitFor(() => {
+      expect(monthInput).toBeDisabled();
+      expect(datePickerDay).toBeDisabled();
+      expect(datePickerYear).toBeDisabled();
+    });
+  });
 });
