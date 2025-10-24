95724b2db1a3385ac785021ee35750627a28784c
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-10-24 14:27:19 -0600

    expose notification card
    Signed-off-by: Vanessa Tran <vanessa.m.tran@gov.ab.ca>

--- libs/react-components/src/experimental/index.ts
+++ libs/react-components/src/experimental/index.ts
@@ -2,3 +2,4 @@ export * from "./resizable-panel/ResizablePanel";
 export * from "../lib/drawer/drawer";
 export * from "./work-side-menu/work-side-menu";
 export * from "./work-side-menu-item/work-side-menu-item";
+export * from "./work-side-notification-card/work-side-notification-card";
