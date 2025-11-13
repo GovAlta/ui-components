beeeeb2c4bcf7379c60375b5411e618b584126b2
Eric Hoff <eric@highfructose.ca>
2025-11-13 16:01:39 -0700

    feat(1908): added angular component for linear-progress

--- libs/react-components/specs/linear-progress.browser.spec.tsx
+++ libs/react-components/specs/linear-progress.browser.spec.tsx
@@ -5,7 +5,7 @@ import { GoabLinearProgress } from "../src";
 
 describe("LinearProgress", () => {
   it("renders with a <goa-linear-progress>", async () => {
-    const { baseElement } = render(<GoabLinearProgress progress={0} />);
+    const { baseElement } = render(<GoabLinearProgress progress={20} />);
     await vi.waitFor(() => {
       const el = baseElement.querySelector("goa-linear-progress");
       expect(el).toBeTruthy();
