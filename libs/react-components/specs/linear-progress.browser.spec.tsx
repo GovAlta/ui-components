7a5372ad4b367b63f9ea561dd0e69264a1c22134
Eric Hoff <eric@highfructose.ca>
2025-11-14 10:16:55 -0700

    feat(1908): added more specs

--- libs/react-components/specs/linear-progress.browser.spec.tsx
+++ libs/react-components/specs/linear-progress.browser.spec.tsx
@@ -8,7 +8,17 @@ describe("LinearProgress", () => {
     const { baseElement } = render(<GoabLinearProgress progress={20} />);
     await vi.waitFor(() => {
       const el = baseElement.querySelector("goa-linear-progress");
+      console.log(el);
       expect(el).toBeTruthy();
+    });
+  });
+
+  it("sets the progress value correctly", async () => {
+    const { baseElement } = render(<GoabLinearProgress progress={75} />);
+    await vi.waitFor(() => {
+      const el = baseElement.querySelector("goa-linear-progress");
+      expect(el?.shadowRoot?.querySelector("progress")?.getAttribute("value")).toBe("75");
+      expect(el?.shadowRoot?.querySelector("span")?.innerText).toBe("75%");
     });
   });
 });
