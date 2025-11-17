8aa953bcaf2375653b2aaf80a703d372b618d54d
Eric Hoff <eric@highfructose.ca>
2025-11-17 14:38:37 -0700

    feat(1908): Add React spec

--- libs/react-components/src/lib/linear-progress/linear-progress.spec.tsx
+++ libs/react-components/src/lib/linear-progress/linear-progress.spec.tsx
@@ -10,4 +10,17 @@ describe("LinearProgress", () => {
       expect(el).toBeTruthy();
     });
   });
+
+  it("passes the prop attribute values correctly", async () => {
+    const testId = "linear-progress-test-id";
+    const progress = 75;
+    const { container } = render(
+      <GoabLinearProgress progress={progress} testId={testId} />,
+    );
+    const el = container.querySelector(`goa-linear-progress[testid="${testId}"]`);
+    await waitFor(() => {
+      expect(el?.getAttribute("testid")).toBe(testId);
+      expect(el?.getAttribute("progress")).toBe(progress.toString());
+    });
+  });
 });
