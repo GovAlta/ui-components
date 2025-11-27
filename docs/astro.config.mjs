b810522790e6a1d908d84ea03c103cd9036ea50b
Chris Olsen <olsen.chris@gmail.com>
2025-11-27 16:43:49 -0700

    add react

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 
+import react from '@astrojs/react';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const workspaceRoot = path.resolve(__dirname, '..');
 
@@ -9,19 +11,24 @@ const workspaceRoot = path.resolve(__dirname, '..');
 export default defineConfig({
   root: '.',
   outDir: '../dist/docs',
+
   server: {
     port: 4203,
     host: '0.0.0.0',
   },
+
   preview: {
     port: 4304,
     host: '0.0.0.0'
   },
+
   vite: {
     server: {
       fs: {
         allow: [workspaceRoot]
       }
     }
-  }
+  },
+
+  integrations: [react()]
 });
