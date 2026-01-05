822e86656d06f550174a114a55840a702be51d12
Chris Olsen <olsen.chris@gmail.com>
2026-01-05 12:08:52 -0700

    re-add the public-form-controller export

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./lib/common";
 export * from "./lib/validators";
+export * from "./lib/public-form-controller";
 export * from "./lib/temporary-notification-controller/temporary-notification-controller";
 export * from "./lib/messaging/messaging";
