6f0210ccf8f2e2319fb0dbbd2fe37efbc56c6e19
Benji Franck <benjamin.franck@gmail.com>
2026-03-19 20:31:52 -0600

    spike: sticky idea 1 - add utility classes

--- libs/web-components/src/index.svelte
+++ libs/web-components/src/index.svelte
@@ -4,6 +4,7 @@
   import "./assets/css/reset.css";
   import "./assets/css/fonts.css";
   import "./assets/css/variables.css";
+  import "./assets/css/utils.css";
   import "./assets/css/components.css";
   import "@abgov/design-tokens/dist/tokens.css";
 </script>
