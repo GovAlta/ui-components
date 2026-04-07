3968af28c6a432c7d7e1df8877e466501a7ab176
Thomas Jeffery <thomaswjeffery@gmail.com>
2026-04-07 16:34:19 -0600

    docs(#3776): update example Figma links to V2 component library

---
+++ docs/src/content/examples/notify-the-user-of-a-future-service-outage/angular.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-notify-the-user-of-a-future-service-outage",
+  templateUrl: "./angular.html",
+})
+export class NotifyTheUserOfAFutureServiceOutageComponent {}
