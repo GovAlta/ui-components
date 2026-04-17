7ea71894ee6e0e269359b97eef2a792c6bb29a81
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2026-04-17 14:28:05 -0600

    chore: test

--- docs/src/components/nav/ParentMenu.tsx
+++ docs/src/components/nav/ParentMenu.tsx
@@ -124,7 +124,7 @@ export function ParentMenu({
   return (
     <>
       <GoabWorkSideMenu
-        heading="Design system"
+        heading="Design system (preview test)"
         url={withBase("/")}
         open={isOpen}
         onToggle={onToggle}
