6cce188243246f8328c6af86fde8eacb1e2c1c87
vanessatran-ddi <vanessatran-ddi@users.noreply.github.com>
2026-04-20 16:29:43 +0000

    Deploy preview for PR 3835 🛫

--- pr-preview/pr-3835/_astro/base-url.DBVvrYwt.js
+++
@@ -1,1 +0,0 @@
-const r="/ui-components/pr-preview/pr-3835/";function i(t){if(!t)return r;if(t.startsWith("http://")||t.startsWith("https://")||t.startsWith("/__"))return t;const s=t.startsWith("/")?t.slice(1):t;return`${r}${s}`}export{i as w};
