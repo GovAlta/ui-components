c66694f3bb0437b103a4b9612b760c690969e1d5
vanessatran-ddi <120135417+vanessatran-ddi@users.noreply.github.com>
2026-06-01 21:39:32 +0000

    Deploying to gh-pages-test from @ GovAlta/ui-components@a9804889de513462466541f87f8e290f76d5e62b 🚀

---
+++ dev/_astro/base-url.jkrg3tWJ.js
@@ -0,0 +1,1 @@
+const s="/ui-components/dev/";function i(t){if(!t)return s;if(t.startsWith("http://")||t.startsWith("https://")||t.startsWith(s))return t;const r=t.startsWith("/")?t.slice(1):t;return`${s}${r}`}export{i as w};
