f7c5c280949f9270fcf1e965d774fc747678ab2d
Mark E <melamatha@gmail.com>
2026-06-25 13:01:03 -0700

    chore(#3893): add playground repro for Block alignment width bug
    Reproduces the reported issue: setting alignment on GoabBlock removes
    flex stretch, so children like GoabFormItem (maxWidth only, no width
    prop) collapse to content width instead of filling the block. Findings
    and proposed direction are in the issue comments, pending a design
    decision before a fix lands.

---
+++ apps/prs/react/src/routes/bugs/bug3893.tsx
@@ -0,0 +1,57 @@
+import {
+  GoabBlock,
+  GoabText,
+  GoabDivider,
+  GoabDetails,
+  GoabLink,
+  GoabFormItem,
+  GoabTextArea,
+} from "@abgov/react-components";
+
+export function Bug3893Route() {
+  return (
+    <div>
+      <GoabText tag="h1" mt="m">
+        Bug #3893: `alignment` property in GoabBlock breaks width
+      </GoabText>
+
+      <GoabBlock>
+        <GoabLink trailingIcon="open">
+          <a href="https://github.com/GovAlta/ui-components/issues/3893" target="_blank" rel="noopener">
+            View on GitHub
+          </a>
+        </GoabLink>
+
+        <GoabDetails heading="Issue Description">
+          <GoabText tag="p">
+            If you setup a GoabBlock component using the alignment property (doesn't seem to matter
+            the value you use), any width properties you set don't seem to matter, it just defaults
+            to an extremely small width.
+          </GoabText>
+        </GoabDetails>
+      </GoabBlock>
+
+      <GoabDivider mt="l" mb="l" />
+
+      <GoabText tag="h2">Test Cases</GoabText>
+
+      <GoabText tag="h3">Test 1: alignment=start, width=100%</GoabText>
+      <GoabBlock direction="column" alignment="start" minWidth="100%" width="100%">
+        <GoabText size="body-l" tag="div">Hello World</GoabText>
+        <GoabFormItem label="Test" maxWidth="100%">
+          <GoabTextArea name="test" value="" />
+        </GoabFormItem>
+      </GoabBlock>
+
+      <GoabText tag="h3">Test 2: alignment=normal (default), width=100%</GoabText>
+      <GoabBlock direction="column" minWidth="100%" width="100%">
+        <GoabText size="body-l" tag="div">Hello World</GoabText>
+        <GoabFormItem label="Test" maxWidth="100%">
+          <GoabTextArea name="test" value="" />
+        </GoabFormItem>
+      </GoabBlock>
+    </div>
+  );
+}
+
+export default Bug3893Route;
