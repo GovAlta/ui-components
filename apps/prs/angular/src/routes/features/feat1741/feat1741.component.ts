30dc603c8e273660e492d25ffec1eb6d1d97c182
Dustin Nielsen <nielsen.dustin@gmail.com>
2026-08-04 19:33:56 -0600

    feat(#1741): Added ariaHidden property to Icon

---
+++ apps/prs/angular/src/routes/features/feat1741/feat1741.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { GoabBlock, GoabIcon, GoabText } from "@abgov/angular-components";
+
+@Component({
+  standalone: true,
+  selector: "abgov-feat1741",
+  templateUrl: "./feat1741.component.html",
+  imports: [GoabBlock, GoabIcon, GoabText],
+})
+export class Feat1741Component {}
